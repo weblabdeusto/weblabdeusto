b2a0af19cbcadc135ac7181f90f878e9213c15af
Luis Rodriguez-Gil <zstars@gmail.com>
2010-04-23 12:11:38 +0000

    VisirTestExperiment server now transforms the login requests that go through it, adding the keepalive=1 attribute that the demo server expects.

--- server/src/weblab/experiment/experiments/visir_test_experiment/VisirTestExperiment.py
+++ server/src/weblab/experiment/experiments/visir_test_experiment/VisirTestExperiment.py
@@ -14,12 +14,20 @@
 # 
 
 import weblab.experiment.Experiment as Experiment
+
 import xml.dom.minidom as minidom
+import httplib
+import urllib
 
 from voodoo.override import Override
 
 
+
+
 class VisirTestExperiment(Experiment.Experiment):
+    
+    MEASURE_SERVER_ADDR = "130.206.138.35:8080"
+    MEASURE_SERVER_TARGET = "/measureserver"
     
     def __init__(self, coord_address, locator, cfg_manager, *args, **kwargs):
         super(VisirTestExperiment, self).__init__(*args, **kwargs)
@@ -31,6 +39,39 @@ class VisirTestExperiment(Experiment.Experiment):
 
     @Override(Experiment.Experiment)
     def do_send_command_to_device(self, command):
+        return self.do_send_command_to_device_real(command)
+    
+    
+    def do_send_command_to_device_real(self, command):
+        command = self.transform_request(command)
+        return self.send_request(command)
+    
+    def transform_request(self, command):
+        print "Transforming request: ", command
+        doc = minidom.parseString(command)
+        tags = doc.getElementsByTagName("login")
+        if len(tags) == 0:
+            return command
+        tag = tags[0]
+        tag.setAttribute("keepalive", "1")
+        command = doc.toxml()
+        return command
+         
+        
+        
+    def send_request(self, request):
+        print "Sending request: ", request
+        conn = httplib.HTTPConnection(VisirTestExperiment.MEASURE_SERVER_ADDR)
+        conn.request("POST", VisirTestExperiment.MEASURE_SERVER_TARGET, request)
+        response = conn.getresponse()
+        print response.status, response.reason
+        data = response.read()
+        conn.close()
+        print "Received response: ", data
+        return data
+
+    
+    def do_send_command_to_device_hc(self, command):
         print "Command: %s" % command
         doc = minidom.parseString(command)
         protocol_tag = doc.getElementsByTagName("protocol")
@@ -41,12 +82,10 @@ class VisirTestExperiment(Experiment.Experiment):
                 return self.handle_response_command(tag)
         return """"""
     
-    
-    def handle_response_command(self, tag):
+    def handle_response_command_hc(self, tag):
         return """<protocol version="1.3"><response/></protocol>"""
 
-
-    def handle_login_command(self, tag):
+    def handle_login_command_hc(self, tag):
         '''
         Handles a login command.
         @param tag The XML "login" tag describing the command.
