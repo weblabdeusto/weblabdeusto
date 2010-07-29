6c107e2a6aa0438ecc9aa734cdb0443ef8b8a384
Jaime Irurzun <jaime@irurzun.com>
2010-07-29 15:01:59 +0000

    Adding the new compulsory properties to every Xilinx experiment instance configuration file.

--- server/launch/sample_balanced1/main_machine/lab_and_experiment/experiment/server_config.py
+++ server/launch/sample_balanced1/main_machine/lab_and_experiment/experiment/server_config.py
@@ -1,6 +1,9 @@
 xilinx_home = "."
 xilinx_impact_full_path = ["python","./tests/unit/weblab/experiment/devices/xilinx_impact/fake_impact.py" ]
 
+xilinx_use_jtag_blazer_to_program = False
+xilinx_use_http_to_send_commands  = False
+
 xilinx_batch_content_FPGA = """setMode -bs
 setCable -port auto
 addDevice -position 1 -file $FILE
