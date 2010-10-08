028062a5f34311342a1b1bf2b660f6c2ff683ea1
Jaime Irurzun <jaime@irurzun.com>
2010-10-08 10:30:13 +0000

    pmock replaced with mocker in LoginAuth tests.

--- server/src/test/unit/weblab/login/LoginAuth.py
+++ server/src/test/unit/weblab/login/LoginAuth.py
@@ -15,7 +15,7 @@
 
 import sys
 import unittest
-import pmock
+import mocker
 
 try:
     import ldap
@@ -33,72 +33,10 @@ invalid_passwd                   = 'invalid_passwd'
 passwd_that_will_raise_exception = 'passwd_that_will_raise_exception'
 uri_that_will_fail               = 'uri that will fail'
 
-def create_ldap_object(valid_user, valid_passwd, invalid_passwd, passwd_that_will_raise_exception):
-    valid_user_plus_domain = valid_user + '@cdk.deusto.es'
-    ldap_object = pmock.Mock()
-    ldap_object.expects(
-            pmock.once()
-        ).simple_bind_s(
-            pmock.eq(valid_user_plus_domain),
-            pmock.eq(valid_passwd)
-        ).will(
-        pmock.return_value(
-            None
-        )
-    )
-    ldap_object.expects(
-            pmock.once()
-        ).simple_bind_s(
-            pmock.eq(valid_user_plus_domain),
-            pmock.eq(invalid_passwd)
-        ).will(
-        pmock.raise_exception(
-            ldap.INVALID_CREDENTIALS("Invalid user/password")
-        )
-    )
-    ldap_object.expects(
-            pmock.once()
-        ).simple_bind_s(
-            pmock.eq(valid_user_plus_domain),   
-            pmock.eq(passwd_that_will_raise_exception)
-        ).will(
-        pmock.raise_exception(
-            Exception("generic exception")
-        )
-    )
-    ldap_object.expects(
-            pmock.once()
-        ).unbind_s()
-    return ldap_object
-
-def create_ldap_module(valid_user, valid_passwd, invalid_passwd, passwd_that_will_raise_exception, uri_that_will_fail):
-    ldap_object = create_ldap_object(valid_user, valid_passwd, invalid_passwd, passwd_that_will_raise_exception)
-
-    ldap_module = pmock.Mock()
-    ldap_module.expects(
-            pmock.once()
-        ).initialize(
-            pmock.eq('ldaps://castor.cdk.deusto.es')
-        ).will(
-            pmock.return_value(
-                ldap_object
-            )
-        )
-    ldap_module.expects(
-            pmock.once()
-        ).initialize(
-            pmock.eq(uri_that_will_fail)
-        ).will(
-            pmock.raise_exception(
-                Exception("fail")
-            )
-        )
-    return ldap_module
-
-
-
-class LoginAuthTestCase(unittest.TestCase):
+class LoginAuthTestCase(mocker.MockerTestCase):
+    
     if LoginAuth.LDAP_AVAILABLE:
+
         def _create_user_auth(self):
             return UserAuth.UserAuth.create_user_auth(
                     UserAuth.LdapUserAuth.NAME,
@@ -129,10 +67,17 @@ class LoginAuthTestCase(unittest.TestCase):
         def test_ldap_login_auth_valid(self):
             user_auth = self._create_user_auth()
             login_auth = LoginAuth.LoginAuth.create(user_auth)
-            ldap_module = create_ldap_module(valid_user, valid_passwd, invalid_passwd, passwd_that_will_raise_exception, uri_that_will_fail)
 
+            ldap_object = self.mocker.mock()
+            ldap_object.simple_bind_s(valid_user + '@cdk.deusto.es', valid_passwd)
+            self.mocker.result(None)
+            ldap_module = self.mocker.mock()
+            ldap_module.initialize('ldaps://castor.cdk.deusto.es')
+            self.mocker.result(ldap_object)
+            ldap_object.unbind_s()
             LoginAuth._ldap_provider.ldap_module = ldap_module
-
+            
+            self.mocker.replay()
             self.assertTrue(
                 login_auth.authenticate(valid_user, valid_passwd)
             )
@@ -140,10 +85,16 @@ class LoginAuthTestCase(unittest.TestCase):
         def test_ldap_login_auth_invalid(self):
             user_auth = self._create_user_auth()
             login_auth = LoginAuth.LoginAuth.create(user_auth)
-            ldap_module = create_ldap_module(valid_user, valid_passwd, invalid_passwd, passwd_that_will_raise_exception, uri_that_will_fail)
 
+            ldap_object = self.mocker.mock()
+            ldap_object.simple_bind_s(valid_user + '@cdk.deusto.es', invalid_passwd)
+            self.mocker.throw(ldap.INVALID_CREDENTIALS("Invalid user/password"))
+            ldap_module = self.mocker.mock()
+            ldap_module.initialize('ldaps://castor.cdk.deusto.es')
+            self.mocker.result(ldap_object)
             LoginAuth._ldap_provider.ldap_module = ldap_module
-
+            
+            self.mocker.replay()
             self.assertFalse(
                 login_auth.authenticate(valid_user, invalid_passwd)
             )
@@ -151,10 +102,16 @@ class LoginAuthTestCase(unittest.TestCase):
         def test_ldap_login_auth_general_exception_binding(self):
             user_auth = self._create_user_auth()
             login_auth = LoginAuth.LoginAuth.create(user_auth)
-            ldap_module = create_ldap_module(valid_user, valid_passwd, invalid_passwd, passwd_that_will_raise_exception, uri_that_will_fail)
 
+            ldap_object = self.mocker.mock()
+            ldap_object.simple_bind_s(valid_user + '@cdk.deusto.es', passwd_that_will_raise_exception)
+            self.mocker.throw(Exception("generic exception"))
+            ldap_module = self.mocker.mock()
+            ldap_module.initialize('ldaps://castor.cdk.deusto.es')
+            self.mocker.result(ldap_object)
             LoginAuth._ldap_provider.ldap_module = ldap_module
 
+            self.mocker.replay()
             self.assertRaises(
                 LoginExceptions.LdapBindingException,
                 login_auth.authenticate,
@@ -166,20 +123,26 @@ class LoginAuthTestCase(unittest.TestCase):
             user_auth = self._create_user_auth()
             user_auth.ldap_uri = uri_that_will_fail
             login_auth = LoginAuth.LoginAuth.create(user_auth)
-            ldap_module = create_ldap_module(valid_user, valid_passwd, invalid_passwd, passwd_that_will_raise_exception, uri_that_will_fail)
-
+            
+            ldap_module = self.mocker.mock()
+            ldap_module.initialize(uri_that_will_fail)
+            self.mocker.throw(Exception("fail"))
             LoginAuth._ldap_provider.ldap_module = ldap_module
 
+            self.mocker.replay()
             self.assertRaises(
                 LoginExceptions.LdapInitializingException,
                 login_auth.authenticate,
                 valid_user, 
                 valid_passwd
             )
+    
     else:
         print >> sys.stderr, "LoginAuth tests skipped since ldap module is not available"
 
+
 class LdapNotAvailableTestCase(OptionalModuleTestCase):
+    
     MODULE    = LoginAuth
     ATTR_NAME = 'LDAP_AVAILABLE'
 
@@ -189,6 +152,7 @@ class LdapNotAvailableTestCase(OptionalModuleTestCase):
             login_auth.authenticate("foo","bar")
 
         self._test_func_without_module(func)
+
 
 def suite():
     return unittest.TestSuite((
