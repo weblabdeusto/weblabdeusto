966ee809b574cf7c19116476d8fb1c38d6c6124b
Luis Rodriguez-Gil <zstars@gmail.com>
2010-11-04 15:54:59 +0000

    [WinRDP] Added AccountManager class. It now changes the password of weblab appropriately through it. Added exception support. Changed namespace and sessionid type as suggested. Admin password specification through a config file isn't supported yet.

--- experiments/unmanaged/vm_services/WindowsRDP/WindowsRDP/Program.cs
+++ experiments/unmanaged/vm_services/WindowsRDP/WindowsRDP/Program.cs
@@ -1,9 +1,64 @@
 ﻿
 using System;
 using System.Net;
+using System.Diagnostics;
+using System.DirectoryServices.AccountManagement;
 
-namespace WindowsRDP
+
+namespace WebLab.VM.WindowsRDP
 {
+
+    class AccountsManager : IDisposable
+    {
+        private PrincipalContext mPrincipalContext;
+
+        public bool Validated { get; private set; }
+
+        /// <summary>
+        /// Retrieves the User with the specified account name. May return null.
+        /// </summary>
+        /// <param name="accName">Account name whose UserPrincipal to retrieve</param>
+        /// <returns>The UserPrincipal object, or null if not found.</returns>
+        private UserPrincipal GetUserPrincipal(string accName)
+        {
+            UserPrincipal principal = UserPrincipal.FindByIdentity(mPrincipalContext, accName);
+            return principal;
+        }
+
+        public AccountsManager(string admin_acc, string admin_pwd)
+        {
+            mPrincipalContext = new PrincipalContext(ContextType.Machine, null, admin_acc, admin_pwd);
+
+            // We need to separatedly validate the credentials
+            bool success = mPrincipalContext.ValidateCredentials(admin_acc, admin_pwd);
+
+            if (!success)
+                throw new Exception("Could not validate credentials");
+
+            Validated = true;
+        }
+
+        public void SetPassword(string acc, string pwd)
+        {
+            var user = GetUserPrincipal(acc);
+            if (user == null)
+                throw new Exception("Could not find such user");
+
+            user.SetPassword(pwd);
+        }
+
+        //public void CreateAccount(string acc, string pwd)
+        //{
+        //    var user = new UserPrincipal(mPrincipalContext, acc, pwd, true);
+        //    user.Save();
+        //    user.Dispose();
+        //}
+
+        public void Dispose()
+        {
+            mPrincipalContext.Dispose();
+        }
+    }
 
     class Handler
     {
@@ -26,11 +81,15 @@ namespace WindowsRDP
         public void ProcessRequest()
         {
             // Write the request we received to console for debugging purposeses.
-            string info = context.Request.HttpMethod + " " + context.Request.Url;
-            Console.WriteLine(info);
+            string info = "Request: " + context.Request.HttpMethod + " " + context.Request.Url;
+            Trace.WriteLine(info);
 
             // Retrieve the session id from the request, which will be used as password.
-            long sessionid = Int64.Parse(context.Request.QueryString["sessionid"]);
+            string sessionid = context.Request.QueryString["sessionid"];
+
+            // Change the password of the weblab user
+            AccountsManager accManager = new AccountsManager("", "");
+            accManager.SetPassword("weblab", sessionid);
         }
     }
 
@@ -39,25 +98,40 @@ namespace WindowsRDP
 	{
 		public static void Main(string[] args)
 		{
+            InitializeLogSystem();
             Program program = new Program();
             program.Run();
 		}
 
 
+        private static void InitializeLogSystem()
+        {
+            TextWriterTraceListener traceListener = new TextWriterTraceListener(System.Console.Out);
+            System.Diagnostics.Trace.Listeners.Add(traceListener);
+        }
+
         public void Run()
         {
-            Console.WriteLine("Starting Windows RDP VM Service");
+            Trace.WriteLine("Starting Windows RDP VM Service");
 
             HttpListener listener = new HttpListener();
             listener.Prefixes.Add("http://*:6789/");
             listener.Start();
-
+			
             while (true)
             {
-                HttpListenerContext ctx = listener.GetContext();
-                Handler handler = new Handler(ctx);
-                handler.ProcessRequest();
-            }
+				try
+				{
+                	HttpListenerContext ctx = listener.GetContext();
+                	Handler handler = new Handler(ctx);
+                	handler.ProcessRequest();
+				}
+				catch(Exception ex)
+				{
+					Trace.WriteLine("Exception caught: ");
+					Trace.WriteLine(ex.Message);
+				}
+			}
         }
 	}
 }
