286cb91fb8219be5b8eec14852b5435317939390
Luis Rodriguez-Gil <zstars@gmail.com>
2010-11-05 15:50:01 +0000

    [VM/WinRDP] Added the service, which makes use of the WRDPLib. Added installer and uninstaller batch scripts, which must be run in admin mode.

---
+++ experiments/unmanaged/vm_services/WindowsRDP/WindowsRDPService/WindowsRDPService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading;
+
+namespace WebLab.VM.WindowsRDP
+{
+    public partial class WindowsRDPService : ServiceBase
+    {
+        private RequestsListener mListener;
+        private Thread mListenerThread;
+
+        public WindowsRDPService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            Trace.WriteLine("WindowsRDPService starting.");
+
+            mListener = new RequestsListener();
+            mListenerThread = new Thread(new ThreadStart(mListener.Run));
+            mListenerThread.Start();
+        }
+
+        protected override void OnStop()
+        {
+            Trace.WriteLine("WindowsRDPService stopping.");
+
+            // This is not particularly advisable, but shouldn't be too bad here.
+            mListenerThread.Abort();
+        }
+    }
+}
