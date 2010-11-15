8fc2a7f9d30671df22334f5b5c0758923b157abe
Luis Rodriguez-Gil <zstars@gmail.com>
2010-11-15 13:24:35 +0000

    [VM/WindowsVNC]: Added project structure, modified project and solution fiels, copied RDP sources.

---
+++ experiments/unmanaged/vm_services/WindowsVNC/WindowsVNCLib/RequestsListener.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Net;
+using System.Diagnostics;
+
+namespace WebLab.VM.WindowsRDP
+{
+
+    /// <summary>
+    /// Listens for HTTP requests according to the ListenerPrefix
+    /// and handles them.
+    /// </summary>
+    public class RequestsListener
+    {
+        public string ListenerPrefix { get; set; }
+
+        private AccountsManager mAccountsManager;
+
+        public RequestsListener(string prefix)
+        {
+            ListenerPrefix = prefix;
+            mAccountsManager = new AccountsManager();
+        }
+
+        public void Run()
+        {
+            try
+            {
+                Trace.WriteLine("Starting Windows RDP VM Service");
+
+                HttpListener listener = new HttpListener();
+                listener.Prefixes.Add(ListenerPrefix);
+                listener.Start();
+
+                while (true)
+                {
+                    try
+                    {
+                        HttpListenerContext ctx = listener.GetContext();
+                        RequestHandler handler = new RequestHandler(ctx, mAccountsManager);
+                        handler.ProcessRequest();
+                    }
+                    catch (Exception ex)
+                    {
+                        Trace.WriteLine("Exception caught: " + ex.Message + "\n" + ex.StackTrace);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Trace.WriteLine("Exception caught @ run: " + e.Message);
+            }
+        }
+    }
+}
---
+++ experiments/unmanaged/vm_services/WindowsVNC/WindowsVNCService/install_service.bat
@@ -0,0 +1,16 @@
+﻿@echo off
+
+REM The following directory is for .NET 2.0
+set DOTNETFX2=%SystemRoot%\Microsoft.NET\Framework\v2.0.50727
+
+REM The following directory is for .NET 4
+set DOTNETFX4=%SystemRoot%\Microsoft.NET\Framework\v4.0.30319
+
+set PATH=%PATH%;%DOTNETFX4%
+
+echo Installing Weblab Windows RDP Service...
+echo ---------------------------------------------------
+InstallUtil /i WindowsRDPService.exe
+echo ---------------------------------------------------
+echo Done.
+pause
