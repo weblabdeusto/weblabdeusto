1e827943c21760bf90495d6b3087eaa4fdd5e56e
Luis Rodriguez-Gil <zstars@gmail.com>
2010-11-15 14:24:09 +0000

    [VM/VNCService] Added IniRW, UltraVNCManager and VNCEncrypt files, which I thought were under version control already.

---
+++ experiments/unmanaged/vm_services/WindowsVNC/WindowsVNCLib/IniRW.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace WebLab.VM.WindowsVNC
+{
+    public class IniRW : IDisposable
+    {
+        public string filePath;
+
+        [DllImport("kernel32")]
+        private static extern long WritePrivateProfileString(string section,
+          string key, string val, string filePath);
+
+        [DllImport("kernel32")]
+        private static extern int GetPrivateProfileString(string section,
+          string key, string def, StringBuilder retVal,
+          int size, string filePath);
+
+        public IniRW(string filePath)
+        {
+            this.filePath = filePath;
+        }
+
+        public void WriteValue(string section, string key, string val)
+        {
+            WritePrivateProfileString(section, key, val, this.filePath);
+        }
+
+        public string ReadValue(string section, string key)
+        {
+            StringBuilder sb = new StringBuilder(1024);
+            GetPrivateProfileString(section, key, "", sb, 1024, this.filePath);
+            return sb.ToString();
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
---
+++ experiments/unmanaged/vm_services/WindowsVNC/WindowsVNCLib/UltraVNCManager.cs
@@ -0,0 +1,50 @@
+﻿
+using System;
+using System.Net;
+using System.Diagnostics;
+using System.DirectoryServices.AccountManagement;
+
+
+namespace WebLab.VM.WindowsVNC
+{
+
+    /// <summary>
+    /// Provides straightforward facilities for the account management
+    /// that we require.
+    /// </summary>
+    public class UltraVNCManager : IDisposable
+    {
+
+        public string UltraVNCPath {get;set;}
+
+        public UltraVNCManager(string ultra_vnc_path)
+        {
+            UltraVNCPath = ultra_vnc_path;
+        }
+
+        public void SetPassword(string pwd)
+        {
+            Trace.WriteLine("Trying to set password... ");
+
+            IniRW rw = new IniRW(UltraVNCPath + "ultravnc.ini");
+            rw.WriteValue("ultravnc", "passwd", pwd);
+
+            Trace.WriteLine("done.");
+        }
+
+        public void SetViewPassword(string pwd)
+        {
+            Trace.WriteLine("Trying to set view password...");
+
+            IniRW rw = new IniRW(UltraVNCPath + "ultravnc.ini");
+            rw.WriteValue("ultravnc", "passwd2", pwd);
+
+            Trace.WriteLine("done.");
+        }
+
+        void IDisposable.Dispose()
+        {
+        }
+    }
+
+}
