5fcf89385567a4c5c849e0cc2303bb1ad05db026
Pablo Orduña <pablo.orduna@deusto.es>
2011-01-25 21:31:59 +0000

    [facebook]  * Showing full_name in uses.py and winners.py

--- server/admin/web/uses.py
+++ server/admin/web/uses.py
@@ -14,17 +14,17 @@ def index(req):
     try:
         cursor = connection.cursor()
         try:
-            SENTENCE = "SELECT u.login, e.name, c.name, uue.start_date, uue.origin " + \
+            SENTENCE = "SELECT u.login, u.full_name, e.name, c.name, uue.start_date, uue.origin " + \
                         "FROM UserUsedExperiment as uue, User as u, Experiment as e, ExperimentCategory as c " + \
                         "WHERE u.id = uue.user_id AND e.id = uue.experiment_id AND e.category_id = c.id " + \
                         "ORDER BY uue.start_date DESC LIMIT %s" % LIMIT
             cursor.execute(SENTENCE)
             elements = cursor.fetchall()
             result = """<html><head><title>Latest uses</title></head><body><table cellspacing="20">
-                        <tr> <td><b>User</b></td> <td><b>Experiment</b></td> <td><b>Date</b></td> <td><b>From </b> </td></tr>
+                        <tr> <td><b>User</b></td> <td><b>Name</b></td> <td><b>Experiment</b></td> <td><b>Date</b></td> <td><b>From </b> </td></tr>
                         """
-            for user_login, experiment_name, category_name, start_date, uue_from in elements:
-                result += "\t<tr> <td> %s </td> <td> %s </td> <td> %s </td> <td> %s </td> </tr>\n" % ( user_login, experiment_name + '@' + category_name, utc2local_str(start_date), uue_from )
+            for user_login, user_full_name, experiment_name, category_name, start_date, uue_from in elements:
+                result += "\t<tr> <td> %s </td> <td> %s </td> <td> %s </td> <td> %s </td> <td> %s </td> </tr>\n" % ( user_login, user_full_name, experiment_name + '@' + category_name, utc2local_str(start_date), uue_from )
         finally: 
             cursor.close()
     finally:
--- server/admin/web/winners.py
+++ server/admin/web/winners.py
@@ -12,14 +12,14 @@ def index(req):
     try:
         cursor = connection.cursor()
         try:
-            SENTENCE = "SELECT u.login, uc.response, uue.end_date, uue.id " + \
+            SENTENCE = "SELECT u.login, u.full_name, uc.response, uue.end_date, uue.id " + \
                         "FROM UserUsedExperiment as uue, User as u, UserCommand as uc " + \
                         "WHERE u.id = uue.user_id AND uue.experiment_id = " + LOGIC_EXP_ID + " AND uc.experiment_use_id = uue.id AND SUBSTRING(uc.response, 1, 2) = 'OK' " + \
                         "ORDER BY uue.experiment_id "
             cursor.execute(SENTENCE)
             elements = cursor.fetchall()
             result = """<html><head><title>Best results</title></head><body><table cellspacing="10">
-                        <tr> <td><b>User</b></td> <td><b>Points</b></td> <td><b>Date</b></td></tr>
+                        <tr> <td><b>User</b></td> <td><b>Name</b></td> <td><b>Points</b></td> <td><b>Date</b></td></tr>
                         """
         finally: 
             cursor.close()
@@ -27,14 +27,14 @@ def index(req):
         connection.close()
 
     final_elements = {}
-    for user_login, points, end_date, use_id in elements:
-        final_elements[use_id] = (user_login, int(points[len('OK: '):]), end_date)
+    for user_login, full_name, points, end_date, use_id in elements:
+        final_elements[use_id] = (user_login, full_name, int(points[len('OK: '):]), end_date)
 
     final_values = final_elements.values()
-    final_values.sort(lambda x, y: -1 * cmp(x[1], y[1]) or cmp(x[2], y[2]))
+    final_values.sort(lambda x, y: -1 * cmp(x[2], y[2]) or cmp(x[3], y[3]))
 
-    for user_login, points, end_date in final_values:
-        result += "\t<tr> <td> %s </td> <td> %s </td> <td> %s </td> </tr>\n" % ( user_login, points, utc2local_str(end_date) )
+    for user_login, full_name, points, end_date in final_values:
+        result += "\t<tr> <td> %s </td> <td> %s </td> <td> %s </td> <td> %s </td> </tr>\n" % ( user_login, full_name, points, utc2local_str(end_date) )
 
     return result + """</table></body></html>"""
 
