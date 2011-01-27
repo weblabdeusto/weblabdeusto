42fa0f6480b250ca0a92125c0177aa1959c1cfc9
Luis Rodriguez-Gil <zstars@gmail.com>
2011-01-27 14:59:08 +0000

    [vm] Updated VM config (Linux one).

--- server/launch/sample/main_machine/main_instance/experiment_vm/server_config.py
+++ server/launch/sample/main_machine/main_instance/experiment_vm/server_config.py
@@ -3,13 +3,13 @@
 
 vm_storage_dir = """C:\Users\lrg\.VirtualBox\Machines"""
 
-vbox_vm_name = "UbuntuVM"
+vbox_vm_name = "UbuntuDefVM2"
+vbox_base_snapshot = "Ready"
 
-#vm_url = "vnc://192.168.51.119:5900"
-vm_url = "vnc://192.168.51.81:5900"
+vm_url = "vnc://192.168.51.51:5901"
 vm_vm_type = "VirtualBox"
 vm_user_manager_type = "HttpQueryUserManager"
 vm_should_store_image = False
-vm_prepare_name = {"""C:\Users\lrg\.VirtualBox\HardDisks\UbuntuVM.vdi""" : """C:\Users\lrg\VBoxOrig\HardDisks\UbuntuVM.vdi"""}
+#vm_prepare_name = {"""C:\Users\lrg\.VirtualBox\HardDisks\UbuntuVM.vdi""" : """C:\Users\lrg\VBoxOrig\HardDisks\UbuntuVM.vdi"""}
 
-http_query_user_manager_url = "http://192.168.51.81:18080"
+http_query_user_manager_url = "http://192.168.51.51:18080"
