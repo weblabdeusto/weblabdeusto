38848fc8c7c2f70ca9837a5101a16ed24edb655e
Pablo Orduña <pablo.orduna@deusto.es>
2011-01-31 01:15:23 +0000

    [client_migrate_to_GWT_2.1]  * Fixing the build. HTMLUnit reported troubles with comments in JSON

--- client/src/es/deusto/weblab/public/configuration_test.js
+++ client/src/es/deusto/weblab/public/configuration_test.js
@@ -5,11 +5,6 @@
 	"intproperty1"     : 15,
 	"wrongintproperty" : "this.is.not.an.int",
 	"experiments" : { 
-		// Experiments not developed in GWT (such as those developed in Flash or Java) don't require
-		// recompiling the client to be loaded. Adding them to this configuration file is enough.
-		// Also those experiments that are inherently reusable for different classes, such as 
-		// those based in Virtual Machines (that will need to handle different experiment names 
-		// and categories) can be added here.
 		"flash" : [
 		           {
 		        	   "experiment.name"     : "flashdummy",
@@ -19,7 +14,6 @@
 		        	   "swf.file"            : "WeblabFlashSample.swf",
 		        	   "message"             : "Note: This is not a real experiment, it's just a demo so as to show that WebLab-Deusto can integrate different web technologies (such as Adobe Flash in this experiment). This demostrates that developing experiments in WebLab-Deusto is really flexible."
                		},
-               		// Other flash experiments could be added here
                ],
         "java" : [
                   {
@@ -30,8 +24,6 @@
                 	  "jar.file"             : "WeblabJavaSample.jar",
                 	  "code"                 : "es.deusto.weblab.client.experiment.plugins.es.deusto.weblab.javadummy.JavaDummyApplet",
                   },
-                  // Other java experiments could be added here
                ],
-        // Other experiments, such as VM or so, could be added here
-}
+	}
 }
