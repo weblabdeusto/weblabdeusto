98b7eed2bef3c932b1c3f0bc2ed581d1a23b24dd
Luis Rodriguez-Gil <zstars@gmail.com>
2011-02-01 16:20:59 +0000

    [vm/services]: Added a generic password changing interface / manager. Renamed more files.

---
+++ experiments/unmanaged/vm_services/WindowsRDPVNC/WindowsRDPLib/IPasswordChanger.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WindowsRDPLib
+{
+
+    /// <summary>
+    /// Exception which will be thrown whenever an error occurs
+    /// while trying to change a password.
+    /// </summary>
+    class PasswordChangingException : Exception
+    {
+        public PasswordChangingException(string msg)
+            : base(msg)
+        {
+        }
+    }
+
+
+    /// <summary>
+    /// Generic interface which should be implemented by
+    /// every class capable of changing the password of some
+    /// system or method. 
+    /// </summary>
+    interface IPasswordChanger
+    {
+
+        /// <summary>
+        /// Changes the system's or method's password.
+        /// Throws a PasswordChangingException on error.
+        /// </summary>
+        /// <param name="newPassword">New password. May need to meet some criteria.
+        /// Dependent on the implementation.</param>
+        void ChangePassword(string newPassword);
+    }
+}
---
+++ experiments/unmanaged/vm_services/WindowsRDPVNC/WindowsRDPLib/PasswordChangerManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WindowsRDPLib
+{
+
+    /// <summary>
+    /// Aids with password changing and changers management. 
+    /// </summary>
+    class PasswordChangerManager
+    {
+        private static PasswordChangerManager mInstance = new PasswordChangerManager();
+        public static PasswordChangerManager Instance { get { return mInstance; } }
+
+        private Dictionary<string, IPasswordChanger> mPasswordChangers = new Dictionary<string, IPasswordChanger>();
+
+        /// <summary>
+        /// Registers a type of password changer.
+        /// </summary>
+        /// <param name="name">Name of the changer. Mostly for tracking/debugging purposes.</param>
+        /// <param name="changer">The changer.</param>
+        public void registerPasswordChanger(string name, IPasswordChanger changer)
+        {
+            mPasswordChangers.Add(name, changer);
+        }
+
+        /// <summary>
+        /// Changes the password of every single registered method/type.
+        /// </summary>
+        /// <param name="newPassword">Password to change everything to.</param>
+        public void changePassword(string newPassword)
+        {
+            foreach (var changer in mPasswordChangers.Values)
+                changer.ChangePassword(newPassword);
+        }
+    }
+}
