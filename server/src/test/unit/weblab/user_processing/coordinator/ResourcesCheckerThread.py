18c4e2d59d6e8f4eeabdc26bb07d01194b42d073
Pablo Orduña <pablo.orduna@deusto.es>
2011-02-27 19:09:00 +0000

    [new_scheduling_system]  * Fixing ResourcesCheckerThread

--- server/src/test/unit/weblab/user_processing/coordinator/ResourcesCheckerThread.py
+++ server/src/test/unit/weblab/user_processing/coordinator/ResourcesCheckerThread.py
@@ -91,6 +91,10 @@ class ResourcesCheckerThreadTestCase(unittest.TestCase):
             RCT.reset()
             try:
                 RCT.set_coordinator(original_coordinator, 10)
+                initial_time = time.time()
+                while not RCT.checker_thread.isAlive() and time.time() - initial_time <= 1:
+                    time.sleep(0.02)
+                # Now it's alive or more than 1 seconds have passed
                 time.sleep(0.02)
                 self.assertTrue(counter > 2) # It's still running
                 self.assertTrue(right_coordinator)
