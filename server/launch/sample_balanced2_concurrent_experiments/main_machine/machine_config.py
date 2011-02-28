4f54d9f18d2b6613cd90e5b95e18ea86c0419340
Pablo Orduña <pablo.orduna@deusto.es>
2011-02-28 06:51:43 +0000

    [sample_balanced2_concurrent_experiments]  * Supporting a new sample_balanced2 configuration on which users use experiments deployed in the same laboratory server

---
+++ server/launch/sample_balanced2_concurrent_experiments/main_machine/machine_config.py
@@ -0,0 +1,141 @@
+#######################
+# Login configuration #
+#######################
+
+weblab_db_username = 'weblab'
+weblab_db_password = 'weblab'
+
+########################################
+# User Processing Server configuration #
+########################################
+
+core_session_type = 'Memory'
+
+core_coordinator_db_username = 'weblab'
+core_coordinator_db_password = 'weblab'
+
+core_coordinator_laboratory_servers = {
+            "laboratory:lab_and_experiment@main_machine" : { 
+                    "exp1|ud-dummy|Dummy experiments" : "dummy1@ud-dummy", 
+                    "exp2|ud-dummy|Dummy experiments" : "dummy2@ud-dummy",
+                    "exp3|ud-dummy|Dummy experiments" : "dummy3@ud-dummy",
+                    "exp4|ud-dummy|Dummy experiments" : "dummy4@ud-dummy",
+                    "exp5|ud-dummy|Dummy experiments" : "dummy5@ud-dummy",
+                    "exp6|ud-dummy|Dummy experiments" : "dummy6@ud-dummy",
+                    "exp7|ud-dummy|Dummy experiments" : "dummy7@ud-dummy",
+                    "exp8|ud-dummy|Dummy experiments" : "dummy8@ud-dummy",
+                    "exp9|ud-dummy|Dummy experiments" : "dummy9@ud-dummy",
+                    "exp10|ud-dummy|Dummy experiments" : "dummy10@ud-dummy",
+                    "exp11|ud-dummy|Dummy experiments" : "dummy11@ud-dummy",
+                    "exp12|ud-dummy|Dummy experiments" : "dummy12@ud-dummy",
+                    "exp13|ud-dummy|Dummy experiments" : "dummy13@ud-dummy",
+                    "exp14|ud-dummy|Dummy experiments" : "dummy14@ud-dummy",
+                    "exp15|ud-dummy|Dummy experiments" : "dummy15@ud-dummy",
+                    "exp16|ud-dummy|Dummy experiments" : "dummy16@ud-dummy",
+                    "exp17|ud-dummy|Dummy experiments" : "dummy17@ud-dummy",
+                    "exp18|ud-dummy|Dummy experiments" : "dummy18@ud-dummy",
+                    "exp19|ud-dummy|Dummy experiments" : "dummy19@ud-dummy",
+                    "exp20|ud-dummy|Dummy experiments" : "dummy20@ud-dummy",
+                    "exp21|ud-dummy|Dummy experiments" : "dummy21@ud-dummy",
+                    "exp22|ud-dummy|Dummy experiments" : "dummy22@ud-dummy",
+                    "exp23|ud-dummy|Dummy experiments" : "dummy23@ud-dummy",
+                    "exp24|ud-dummy|Dummy experiments" : "dummy24@ud-dummy",
+                    "exp25|ud-dummy|Dummy experiments" : "dummy25@ud-dummy",
+                    "exp26|ud-dummy|Dummy experiments" : "dummy26@ud-dummy",
+                    "exp27|ud-dummy|Dummy experiments" : "dummy27@ud-dummy",
+                    "exp28|ud-dummy|Dummy experiments" : "dummy28@ud-dummy",
+                    "exp29|ud-dummy|Dummy experiments" : "dummy29@ud-dummy",
+                    "exp30|ud-dummy|Dummy experiments" : "dummy30@ud-dummy",
+                    "exp31|ud-dummy|Dummy experiments" : "dummy31@ud-dummy",
+                    "exp32|ud-dummy|Dummy experiments" : "dummy32@ud-dummy",
+                    "exp33|ud-dummy|Dummy experiments" : "dummy33@ud-dummy",
+                    "exp34|ud-dummy|Dummy experiments" : "dummy34@ud-dummy",
+                    "exp35|ud-dummy|Dummy experiments" : "dummy35@ud-dummy",
+                    "exp36|ud-dummy|Dummy experiments" : "dummy36@ud-dummy",
+                    "exp37|ud-dummy|Dummy experiments" : "dummy37@ud-dummy",
+                    "exp38|ud-dummy|Dummy experiments" : "dummy38@ud-dummy",
+                    "exp39|ud-dummy|Dummy experiments" : "dummy39@ud-dummy",
+                    "exp40|ud-dummy|Dummy experiments" : "dummy40@ud-dummy",
+                    "exp41|ud-dummy|Dummy experiments" : "dummy41@ud-dummy",
+                    "exp42|ud-dummy|Dummy experiments" : "dummy42@ud-dummy",
+                    "exp43|ud-dummy|Dummy experiments" : "dummy43@ud-dummy",
+                    "exp44|ud-dummy|Dummy experiments" : "dummy44@ud-dummy",
+                    "exp45|ud-dummy|Dummy experiments" : "dummy45@ud-dummy",
+                    "exp46|ud-dummy|Dummy experiments" : "dummy46@ud-dummy",
+                    "exp47|ud-dummy|Dummy experiments" : "dummy47@ud-dummy",
+                    "exp48|ud-dummy|Dummy experiments" : "dummy48@ud-dummy",
+                    "exp49|ud-dummy|Dummy experiments" : "dummy49@ud-dummy",
+                    "exp50|ud-dummy|Dummy experiments" : "dummy50@ud-dummy",
+                    "exp51|ud-dummy|Dummy experiments" : "dummy51@ud-dummy",
+                    "exp52|ud-dummy|Dummy experiments" : "dummy52@ud-dummy",
+                    "exp53|ud-dummy|Dummy experiments" : "dummy53@ud-dummy",
+                    "exp54|ud-dummy|Dummy experiments" : "dummy54@ud-dummy",
+                    "exp55|ud-dummy|Dummy experiments" : "dummy55@ud-dummy",
+                    "exp56|ud-dummy|Dummy experiments" : "dummy56@ud-dummy",
+                    "exp57|ud-dummy|Dummy experiments" : "dummy57@ud-dummy",
+                    "exp58|ud-dummy|Dummy experiments" : "dummy58@ud-dummy",
+                    "exp59|ud-dummy|Dummy experiments" : "dummy59@ud-dummy",
+                    "exp60|ud-dummy|Dummy experiments" : "dummy60@ud-dummy",
+                    "exp61|ud-dummy|Dummy experiments" : "dummy61@ud-dummy",
+                    "exp62|ud-dummy|Dummy experiments" : "dummy62@ud-dummy",
+                    "exp63|ud-dummy|Dummy experiments" : "dummy63@ud-dummy",
+                    "exp64|ud-dummy|Dummy experiments" : "dummy64@ud-dummy",
+                    "exp65|ud-dummy|Dummy experiments" : "dummy65@ud-dummy",
+                    "exp66|ud-dummy|Dummy experiments" : "dummy66@ud-dummy",
+                    "exp67|ud-dummy|Dummy experiments" : "dummy67@ud-dummy",
+                    "exp68|ud-dummy|Dummy experiments" : "dummy68@ud-dummy",
+                    "exp69|ud-dummy|Dummy experiments" : "dummy69@ud-dummy",
+                    "exp70|ud-dummy|Dummy experiments" : "dummy70@ud-dummy",
+                    "exp71|ud-dummy|Dummy experiments" : "dummy71@ud-dummy",
+                    "exp72|ud-dummy|Dummy experiments" : "dummy72@ud-dummy",
+                    "exp73|ud-dummy|Dummy experiments" : "dummy73@ud-dummy",
+                    "exp74|ud-dummy|Dummy experiments" : "dummy74@ud-dummy",
+                    "exp75|ud-dummy|Dummy experiments" : "dummy75@ud-dummy",
+                    "exp76|ud-dummy|Dummy experiments" : "dummy76@ud-dummy",
+                    "exp77|ud-dummy|Dummy experiments" : "dummy77@ud-dummy",
+                    "exp78|ud-dummy|Dummy experiments" : "dummy78@ud-dummy",
+                    "exp79|ud-dummy|Dummy experiments" : "dummy79@ud-dummy",
+                    "exp80|ud-dummy|Dummy experiments" : "dummy80@ud-dummy",
+            }
+        }
+
+core_scheduling_systems = {
+        "ud-fpga"    : ("PRIORITY_QUEUE", {}),
+        "ud-pld"     : ("PRIORITY_QUEUE", {}),
+        "ud-gpib"    : ("PRIORITY_QUEUE", {}),
+        "ud-pic"     : ("PRIORITY_QUEUE", {}),
+        "ud-dummy"   : ("PRIORITY_QUEUE", {}),
+        "ud-logic"   : ("PRIORITY_QUEUE", {}),
+        "flashdummy" : ("PRIORITY_QUEUE", {}),
+        "javadummy"  : ("PRIORITY_QUEUE", {}),
+        "visirtest"  : ("PRIORITY_QUEUE", {}),
+    }
+
+
+##########################
+# Database configuration #
+##########################
+
+
+core_db_users_student_read_username         = 'wl_student_read'
+core_db_users_student_read_password         = 'wl_student_read_password'
+core_db_users_student_write_username        = 'wl_student_write'
+core_db_users_student_write_password        = 'wl_student_write_password'
+core_db_users_professor_read_username       = 'wl_prof_read'
+core_db_users_professor_read_password       = 'wl_prof_read_password'
+core_db_users_professor_write_username      = 'wl_prof_write'
+core_db_users_professor_write_password      = 'wl_prof_write_password'
+core_db_users_administrator_read_username   = 'wl_admin_read'
+core_db_users_administrator_read_password   = 'wl_admin_read_password'
+core_db_users_administrator_write_username  = 'wl_admin_write'
+core_db_users_administrator_write_password  = 'wl_admin_write_password'
+core_db_users_externalentity_read_username  = 'wl_exter_read'
+core_db_users_externalentity_read_password  = 'wl_exter_read_password'
+core_db_users_externalentity_write_username     = 'wl_exter_write'
+core_db_users_externalentity_write_password     = 'wl_exter_write_password'
+
+db_driver        = "MySQLdb"
+db_host          = "localhost"
+db_database = "WebLabTests"
+db_prefix        = "wl_"
+
---
+++ server/launch/sample_balanced2_concurrent_experiments/main_machine/main_instance3/core/server_config.py
@@ -0,0 +1,21 @@
+##############################
+# RemoteFacade configuration #
+##############################
+
+core_coordinator_clean = False
+
+core_facade_soap_bind       = ''
+core_facade_soap_port         = 30123
+core_facade_soap_service_name = '/weblab/soap/'
+
+core_facade_xmlrpc_bind    = ''
+core_facade_xmlrpc_port      = 39345
+
+core_facade_json_bind      = ''
+core_facade_json_port        = 38345
+
+admin_facade_json_port        = 38545
+
+core_facade_server_route     = 'route3'
+
+
