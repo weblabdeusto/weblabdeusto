7a7c9c62e2a65898d589686142b690e16c32fa13
Luis Rodriguez-Gil <zstars@gmail.com>
2011-06-28 09:20:50 +0000

    [server/send_file_async]: Added a new optional parameter to the python FileUploading script to support async sending of a file.

--- server/src/weblab/user_processing/facade/web/FileUpload.py
+++ server/src/weblab/user_processing/facade/web/FileUpload.py
@@ -11,6 +11,7 @@
 # listed below:
 #
 # Author: Pablo Orduña <pablo@ordunya.com>
+#         Luis Rodriguez <luis.rodriguez@opendeusto.es>
 # 
 
 from weblab.facade.RemoteFacadeManagerCodes import WEBLAB_GENERAL_EXCEPTION_CODE, PYTHON_GENERAL_EXCEPTION_CODE
@@ -21,6 +22,7 @@ import weblab.experiment.Util as Util
 FILE_SENT = 'file_sent'
 FILE_INFO = 'file_info'
 SESSION_ID = 'session_id'
+IS_ASYNC = 'is_async'
 
 BASE_HTML_TEMPLATE="""<?xml version="1.0"?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
@@ -44,11 +46,23 @@ class FileUploadMethod(WebFacadeServer.Method):
     path = '/upload/'
 
     def run(self):
+        """
+        run()
+        Handles file uploading. It will extract the required parameters FILE_SENT, FILE_INFO,
+        SESSION_ID, and the optional parameter IS_ASYNC, and call either send_file or 
+        send_async_file depending on this last one.
+        @return HTML defined above, with the success or failure response.
+        """
         try:
-            file_info, file_sent, session_id = self._check_arguments()
+            file_info, file_sent, session_id, is_async = self._check_arguments()
             file_content = Util.serialize(file_sent)
             sid = SessionId(session_id)
-            result = self.server.send_file(sid, file_content, file_info)
+            
+            if(not is_async):
+                result = self.server.send_file(sid, file_content, file_info)
+            else:
+                result = self.server.send_async_file(sid, file_content, file_info)
+                
         except FileUploadException, fue:
             code, message = fue.args
             return FAULT_HTML_TEMPLATE % {
@@ -67,6 +81,12 @@ class FileUploadMethod(WebFacadeServer.Method):
                     }
 
     def _check_arguments(self):
+        """
+        _check_arguments()
+        Retrieves the arguments which describe the file being sent.
+        These areguments are the file_sent, the file-info, the sessionid, and optionally
+        @return file_info, file_sent, session_id, is_async
+        """
         file_info = self.get_argument(FILE_INFO)
         if file_info is None:
             raise FileUploadException(WEBLAB_GENERAL_EXCEPTION_CODE, "%s argument not provided!" % FILE_INFO)
@@ -76,7 +96,21 @@ class FileUploadMethod(WebFacadeServer.Method):
         session_id = self.get_argument(SESSION_ID)
         if session_id is None:
             raise FileUploadException(WEBLAB_GENERAL_EXCEPTION_CODE, "%s argument not provided!" % SESSION_ID)
-        return file_info, file_sent, session_id 
+        
+        # Read the IS_ASYNC parameter, which will indicate us whether we should execute the send_file asynchronously
+        # or synchronously. 
+        is_async_str = self.get_argument(IS_ASYNC)
+        if is_async_str is None:
+            is_async = False
+        else:
+            is_async_str = is_async_str.tolower()
+            if is_async_str in ("true", "yes", "1"):
+                is_async = True
+            elif is_async_str in ("false", "no", "0"):
+                is_async = False
+            else:
+                raise FileUploadException(WEBLAB_GENERAL_EXCEPTION_CODE, "%s argument is present but not valid (should be a boolean)!" % IS_ASYNC)
+        return file_info, file_sent, session_id, is_async
 
 class FileUploadException(Exception):
     pass
