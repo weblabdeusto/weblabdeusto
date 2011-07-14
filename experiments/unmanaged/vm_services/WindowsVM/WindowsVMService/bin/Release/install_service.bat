73f6d99a88a5d64a09d9b06b766f94b7dea5571d
Pablo Orduña <pablo.orduna@deusto.es>
2011-07-14 18:35:14 +0000

    [vm_services]  * Commiting uncommited code adding the DOTNETFX2 path to the install_service.bat

--- experiments/unmanaged/vm_services/WindowsVM/WindowsVMService/bin/Release/install_service.bat
+++ experiments/unmanaged/vm_services/WindowsVM/WindowsVMService/bin/Release/install_service.bat
@@ -10,7 +10,7 @@ set DOTNETFX2=%SystemRoot%\Microsoft.NET\Framework\v2.0.50727
 REM The following directory is for .NET 4
 set DOTNETFX4=%SystemRoot%\Microsoft.NET\Framework\v4.0.30319
 
-set PATH=%PATH%;%DOTNETFX4%
+set PATH=%PATH%;%DOTNETFX4%;%DOTNETFX2%
 
 echo Installing Weblab Windows VM Service...
 echo ---------------------------------------------------
