cf928a9cece0528f2246dd5c297feb788a5f1196
Pablo Orduña <pablo.orduna@deusto.es>
2011-07-18 15:09:24 +0000

    [labview]  * Current default version => LabVIEW 2010

--- client/src/es/deusto/weblab/client/lab/experiments/plugins/es/deusto/weblab/labview/ui/LabViewVersionArguments.java
+++ client/src/es/deusto/weblab/client/lab/experiments/plugins/es/deusto/weblab/labview/ui/LabViewVersionArguments.java
@@ -24,10 +24,15 @@ class LabViewVersionArguments{
 	
 	private static Map<String, LabViewVersionArguments> byVersion = new HashMap<String, LabViewVersionArguments>();
 	static{
-		final String classId = "CLSID:A40B0AD4-B50E-4E58-8A1D-8544233807B1";
-		final String codeBase = "ftp://ftp.ni.com/support/labview/runtime/windows/9.0/LVRTE90min.exe";
-		final String pluginsPage = "http://digital.ni.com/express.nsf/bycode/exck2m";
-		byVersion.put("2009", new LabViewVersionArguments(classId, codeBase, pluginsPage)); 
+		final String classId_2009 = "CLSID:A40B0AD4-B50E-4E58-8A1D-8544233807B1";
+		final String codeBase_2009 = "ftp://ftp.ni.com/support/labview/runtime/windows/9.0/LVRTE90min.exe";
+		final String pluginsPage_2009 = "http://digital.ni.com/express.nsf/bycode/exck2m";
+		byVersion.put("2009", new LabViewVersionArguments(classId_2009, codeBase_2009, pluginsPage_2009));
+		
+		final String classId_2010 = "CLSID:A40B0AD4-B50E-4E58-8A1D-8544233807B2";
+		final String codeBase_2010 = "ftp://ftp.ni.com/support/labview/runtime/windows/2010/LVRTE2010min.exe";
+		final String pluginsPage_2010 = "http://digital.ni.com/express.nsf/bycode/exck2m";
+		byVersion.put("2010", new LabViewVersionArguments(classId_2010, codeBase_2010, pluginsPage_2010));
 	}
 	
 	static LabViewVersionArguments getVersion(String version){
--- server/launch/sample/main_machine/main_instance/experiment_blinkled/server_config.py
+++ server/launch/sample/main_machine/main_instance/experiment_blinkled/server_config.py
@@ -2,3 +2,4 @@
 #-*-*- encoding: utf-8 -*-*-
 
 labview_filename = "PLEASE SET THIS"
+labview_copyfile = False
