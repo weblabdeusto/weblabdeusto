c03b41c69cb8d7d423e07bb44f29223a1c0fdb2e
Pablo Orduña <pablo.orduna@deusto.es>
2011-07-19 18:25:50 +0000

    [labview]  * Adding a programmer that works with XML-RPC and whenever it receives a file it sends stores it locally

---
+++ server/src/weblab/experiment/experiments/labview_experiment/fpga_programmer/programmer.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2005-2009 University of Deusto
+# All rights reserved.
+#
+# This software is licensed as described in the file COPYING, which
+# you should have received as part of this distribution.
+#
+# This software consists of contributions made by many individuals, 
+# listed below:
+#
+# Author: Pablo Orduña <pablo@ordunya.com>
+# 
+
+import base64
+import tempfile
+import traceback
+import SimpleXMLRPCServer
+
+import sys, os
+sys.path.append(os.sep.join(('..','..','..','..','..')))
+
+import libraries
+libraries.load()
+
+import voodoo.configuration.ConfigurationManager as CM
+import weblab.experiment.devices.xilinx_impact.XilinxDevices as XilinxDevices
+import weblab.experiment.devices.xilinx_impact.XilinxImpact  as XilinxImpact
+
+class FpgaProgrammer(object):
+    def program(self, serialized_content):
+        print "File received"
+        content = base64.decodestring(serialized_content)
+
+        fd, file_name = tempfile.mkstemp(prefix='weblab_fpga_program', suffix='.bit')
+        try:
+            os.write(fd, content)
+            os.close(fd)
+
+            print "Programming... %s" % file_name
+            impact.program_device(file_name)
+
+            print "Programmed successfully"
+            return "ok"
+        except:
+            traceback.print_exc()
+            raise
+        finally:
+            os.remove(file_name)
+
+if __name__ == '__main__':
+    cfg_manager = CM.ConfigurationManager()
+    cfg_manager.append_path("config.py")
+    impact = XilinxImpact.create(XilinxDevices.FPGA, cfg_manager)
+
+    server = SimpleXMLRPCServer.SimpleXMLRPCServer(('', cfg_manager.get_value("port")))
+    server.register_instance(FpgaProgrammer())
+    server.serve_forever()
