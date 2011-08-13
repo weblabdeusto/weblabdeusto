69c72655dc8f1f7e9b7e2bdb86714366ac12c854
Jaime Irurzun <jaime@irurzun.com>
2011-08-14 00:56:55 +0100

    Fix import in serial_port test

--- server/src/test/unit/weblab/experiment/devices/serial_port.py
+++ server/src/test/unit/weblab/experiment/devices/serial_port.py
@@ -7,24 +7,24 @@
 # This software is licensed as described in the file COPYING, which
 # you should have received as part of this distribution.
 #
-# This software consists of contributions made by many individuals, 
+# This software consists of contributions made by many individuals,
 # listed below:
 #
 # Author: Jaime Irurzun <jaime.irurzun@gmail.com>
-# 
+#
 
 import unittest
 
 import test.util.optional_modules as optional_modules
-from weblab.experiment.devices.serial_port import SerialPort
+from weblab.experiment.devices import serial_port
 
 class SerialPortTestCase(optional_modules.OptionalModuleTestCase):
-    MODULE    = SerialPort
+    MODULE    = serial_port
     ATTR_NAME = 'SERIAL_AVAILABLE'
 
     def test_serial_not_available(self):
         def func():
-            serial = SerialPort()
+            serial = serial_port.SerialPort()
             serial.open_serial_port(1)
             serial.send_code(65)
             serial.close_serial_port()
