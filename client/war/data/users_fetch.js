edb6de63746ade6351c50732816f442bc7d5c903
Luis Rodriguez-Gil <zstars@gmail.com>
2011-09-14 17:36:29 +0200

    [admin_panel/users]: Added apparently missing smiley image, modified the test users_fetch.js file to not have an id field anymore.

--- client/war/data/users_fetch.js
+++ client/war/data/users_fetch.js
@@ -4,7 +4,6 @@
 		"data":
 		[
 			{
-				"id": 1,
 				"login": "student1",
 				"full_name": "First Name",
 				"email": "student1@students.com",
@@ -12,7 +11,6 @@
 				"role": "teacher"
 			},
 			{
-				"id": 2,
 				"login": "student2",
 				"full_name": "Second Name",
 				"email": "student2@students.com",
@@ -20,7 +18,6 @@
 				"role": "student"
 			},
 			{
-				"id": 3,
 				"login": "student3",
 				"full_name": "Third Name",
 				"email": "student3@students.com",
@@ -28,7 +25,6 @@
 				"role": "administrator"
 			},
 			{
-				"id": 4,
 				"login": "student4",
 				"full_name": "Fourth Name",
 				"email": "student4@students.com",
