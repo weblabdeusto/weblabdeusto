112d078a029990ce0559aae2cc5dc0bb995e84ad
Luis Rodriguez-Gil <zstars@gmail.com>
2011-09-15 09:31:57 +0200

    [admin_panel/users]: Removed id parameter from the PermissionType data source and test file.

--- client/war/data/permission_types_fetch.js
+++ client/war/data/permission_types_fetch.js
@@ -4,25 +4,21 @@
 		"data":
 		[
 			{
-				"id": 1,
 				"name": "exp_allowed",
 				"datatype": "string",
 				"description": "Permission to use a specified experiment"
 			},
 			{
-				"id": 2,
 				"name": "admin_panel_access",
 				"datatype": "string",
 				"description": "Permission to access the administration panel"
 			},
-						{
-				"id": 3,
+			{
 				"name": "whatever_allowed",
 				"datatype": "float",
 				"description": "First perm type"
 			},
-						{
-				"id": 4,
+			{
 				"name": "testing_allowed",
 				"datatype": "bool",
 				"description": "First perm type"
