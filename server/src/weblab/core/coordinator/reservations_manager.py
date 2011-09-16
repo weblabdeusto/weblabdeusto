e3072d5ddd6fc4db9de781f778105ee4d3309dfc
Pablo Orduña <pablo.orduna@deusto.es>
2011-09-16 10:47:29 -0400

    Remove the PendingToFinishReservation rows (didn't make commit :-D)

--- server/src/weblab/core/coordinator/reservations_manager.py
+++ server/src/weblab/core/coordinator/reservations_manager.py
@@ -126,6 +126,7 @@ class ReservationsManager(object):
             pending_to_finish = session.query(PendingToFinishReservation).filter_by(id=reservation_id).first()
             if pending_to_finish is not None:
                 session.delete(pending_to_finish)
+            session.commit()
         finally:
             session.close()
 
