e5995e309d8ce5512bd7b673d117727a3037d3fd
Pablo Orduña <pablo.orduna@deusto.es>
2011-09-21 19:40:11 +0200

    Add to admin.cli.migration the access_forward and the new arguments

--- server/admin/cli/migrate_db_40m1_to_40m2/migrate.py
+++ server/admin/cli/migrate_db_40m1_to_40m2/migrate.py
@@ -18,6 +18,46 @@ class AddingPriorityToPermissionParameterPatch(Patch):
         parameter = Model.DbPermissionTypeParameter(permission_type = experiment_allowed, name = 'priority' , datatype = 'int', description = 'Priority (the lower value the higher priority)')
         session.add(parameter)
 
+class AddingInitializationInAccountingToPermissionParameterPatch(Patch):
+
+    CHECK_FORMAT = Patch.SQLALCHEMY_FORMAT
+    APPLY_FORMAT = Patch.SQLALCHEMY_FORMAT
+
+    def check(self, session):
+        # Check if the 'experiment_allowed' permission type has the 'initialization_in_accounting' parameter
+        return len([ parameter 
+                    for parameter in session.query(Model.DbPermissionType).filter_by(name = 'experiment_allowed').first().parameters
+                    if parameter.name == 'initialization_in_accounting']) == 0
+
+    def apply(self, session):
+        experiment_allowed = session.query(Model.DbPermissionType).filter_by(name = 'experiment_allowed').first()
+        parameter = Model.DbPermissionTypeParameter(permission_type = experiment_allowed, name = 'initialization_in_accounting' , datatype = 'bool', description = 'time_allowed, should count with the initialization time or not?')
+        session.add(parameter)
+
+class AddingAccessForwardToPermissionsPatch(Patch):
+
+    CHECK_FORMAT = Patch.SQLALCHEMY_FORMAT
+    APPLY_FORMAT = Patch.SQLALCHEMY_FORMAT
+
+    def check(self, session):
+        # Check if the 'experiment_allowed' permission type has the 'initialization_in_accounting' parameter
+        return session.query(Model.DbPermissionType).filter_by(name = 'access_forward').first() is None
+
+    def apply(self, session):
+        
+        user_applicable_permission  = Model.DbUserApplicablePermissionType()
+        group_applicable_permission = Model.DbGroupApplicablePermissionType()
+        role_applicable_permission  = Model.DbRoleApplicablePermissionType()
+        ee_applicable_permission    = Model.DbExternalEntityApplicablePermissionType()
+
+        session.add(user_applicable_permission)
+        session.add(group_applicable_permission)
+        session.add(role_applicable_permission)
+        session.add(ee_applicable_permission)
+
+        permission_type = Model.DbPermissionType('access_forward',"Users with this permission will be allowed to forward reservations to other external users.", user_applicable_permission, role_applicable_permission, group_applicable_permission, ee_applicable_permission)
+        session.add(permission_type)
+
 class AddingReservationIdToUserUsedExperiment(Patch):
 
     table_name = 'UserUsedExperiment'
@@ -36,6 +76,8 @@ class AddingReservationIdToEntityUsedExperiment(AddingReservationIdToUserUsedExp
 if __name__ == '__main__':
     applier = PatchApplier("weblab", "weblab", "WebLabTests", [
                                 AddingPriorityToPermissionParameterPatch, 
+                                AddingInitializationInAccountingToPermissionParameterPatch,
+                                AddingAccessForwardToPermissionsPatch,
                                 AddingReservationIdToUserUsedExperiment, 
                                 AddingReservationIdToEntityUsedExperiment
                             ])
