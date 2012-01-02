1e3583f22227d6198154581ed641cfca9b3561a1
Pablo Orduña <pablo.orduna@deusto.es>
2012-01-02 22:31:05 +0100

    Testing sqlite on launch/sample

--- server/launch/sample/global_config.py
+++ server/launch/sample/global_config.py
@@ -1,2 +1,9 @@
 server_admin = 'weblab@deusto.es'
 debug_mode   = True
+
+db_engine = 'sqlite'
+# db_engine = 'mysql'
+core_coordinator_db_engine     = db_engine
+session_sqlalchemy_engine      = db_engine
+session_lock_sqlalchemy_engine = db_engine
+
