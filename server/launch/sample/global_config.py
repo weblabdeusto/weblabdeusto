db58f120f2668fa0c11812ac3b9cd01658031f4d
Pablo Orduña <pablo.orduna@deusto.es>
2012-01-02 23:49:30 +0100

    Restablish mysql as primary db provider

--- server/launch/sample/global_config.py
+++ server/launch/sample/global_config.py
@@ -1,8 +1,8 @@
 server_admin = 'weblab@deusto.es'
 debug_mode   = True
 
-db_engine = 'sqlite'
-# db_engine = 'mysql'
+# db_engine = 'sqlite'
+db_engine = 'mysql'
 core_coordinator_db_engine     = db_engine
 session_sqlalchemy_engine      = db_engine
 session_lock_sqlalchemy_engine = db_engine
