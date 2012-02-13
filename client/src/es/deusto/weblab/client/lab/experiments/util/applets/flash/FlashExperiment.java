843d3c161ebcefe800150e67c60ca61369c0808d
Pablo Orduña <pablo.orduna@deusto.es>
2012-02-13 13:58:56 +0100

    Fix bug in Google Chrome 17. See http://code.google.com/p/chromium/issues/detail?id=108228#c12

--- client/src/es/deusto/weblab/client/lab/experiments/util/applets/flash/FlashExperiment.java
+++ client/src/es/deusto/weblab/client/lab/experiments/util/applets/flash/FlashExperiment.java
@@ -293,7 +293,7 @@ public class FlashExperiment extends AbstractExternalAppBasedBoard{
 		var flashHtml    = "<object classid=\"clsid:D27CDB6E-AE6D-11cf-96B8-444553540000\" type=\"application/x-shockwave-flash\" width=\"" + width + "\" height=\"" + height + "\" id=\"flashobj\">" + 
 								"<param name=\"movie\" value=\"" + swfFile + "\" id\"flash_emb\"/>" + 
 								"<param name=\"flashvars\" value=\"" + flashvars + "\"/>" + 
-								"<embed src=\"" + swfFile + "\" width=\"" + width + "\" height=\"" + height + "\" flashvars=\"" + flashvars + "\"   />" + 
+								"<embed type=\"application/x-shockwave-flash\" src=\"" + swfFile + "\" width=\"" + width + "\" height=\"" + height + "\" flashvars=\"" + flashvars + "\"   />" + 
 							"</object>";
 		
 		var completeHtml = "<html>" +
