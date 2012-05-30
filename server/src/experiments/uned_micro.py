0e7079602e288b10be514a2bb22ddbc5cfebbfcd
Pablo Orduña <pablo.orduna@deusto.es>
2012-05-30 20:23:48 +0200

    Implement the experiment server for the UNED micro

--- server/src/experiments/uned_micro.py
+++ server/src/experiments/uned_micro.py
@@ -13,10 +13,17 @@
 # Author: Pablo Orduña <pablo@ordunya.com>
 #
 
+import cgi
 import urllib
 import urllib2
 import cookielib
 
+import json
+
+import weblab.experiment.level as ExperimentApiLevel
+from voodoo.log import logged
+from voodoo.override import Override
+from weblab.experiment.experiment import Experiment
 
 ACCOUNT_URL = 'MC68KLab/Account/' 
 
@@ -142,6 +149,88 @@ class UnedMicroClient(object):
         <input type="submit" value="Añadir" style="float: left" class="optionBoardColors" />
         """
 
+class MicroprocessorExperiment(Experiment):
+
+    def __init__(self, coord_address, locator, cfg_manager, *args, **kwargs):
+        super(MicroprocessorExperiment, self).__init__(*args, **kwargs)
+        self._cfg_manager    = cfg_manager
+
+        self.debug           = self._cfg_manager.get_value('debug', True)
+        self.admin_username  = self._cfg_manager.get_value('microprocessor_admin_username',  '')
+        self.admin_password  = self._cfg_manager.get_value('microprocessor_admin_password',  '')
+        self.domain          = self._cfg_manager.get_value('microprocessor_domain', '62.204.201.32:90')
+        self.global_password = self._cfg_manager.get_value('microprocessor_global_password', '')
+
+        self.create_client = lambda : UnedMicroClient(self.domain, self.admin_username, self.admin_password)
+       
+        # 
+        # TODO: right now the experiment does not know anything about the deserialized_server_initial_data
+        # does not include the username, so we can not dynamically create the user.
+        # 
+        self.real_username   = self._cfg_manager.get_value('microprocessor_username', '')
+
+    def do_get_api(self):
+        return ExperimentApiLevel.level_2
+
+    @Override(Experiment)
+    @logged("info")
+    def do_start_experiment(self, serialized_client_initial_data, serialized_server_initial_data):
+        """
+        Callback run when the experiment is started.
+        """
+        if self.debug:
+            print "[Microprocessor*] do_start_experiment called"
+
+        server_initial_data = json.loads(serialized_server_initial_data)
+        user_identifier = server_initial_data.get('weblab.username', self.real_username)
+
+        client = self.create_client()
+
+        # Attempt to register the user if he did not exist
+        client.register_student(user_identifier, user_identifier, self.global_password)
+
+        current_config = {}
+
+        # Log in as this user
+        try:
+            self.session_id = client.login(user_identifier, password)
+        except Exception as e:
+            current_config['error'] = True
+            current_config['msg']   = cgi.escape(str(e))
+        else:
+            url = client.create_url(self.session_id)
+            current_config['url']   = url
+            current_config['error'] = False
+
+        return json.dumps({ "initial_configuration" : json.dumps(current_config), "batch" : False })
+
+    @Override(Experiment.Experiment)
+    def do_should_finish(self):
+        session_id = self.session_id
+        if session_id is None:
+            return -1 # Finished
+
+        client = self.create_client()
+        if client.is_still_logged(session_id):
+            return 5 # Ask again in 5 seconds
+
+        return -1 # Not active. Finish this session
+
+    @Override(Experiment)
+    @logged("info")
+    def do_dispose(self):
+        """
+        Callback to perform cleaning after the experiment ends.
+        """
+        if self.debug:
+            print "[Microprocessor*] do_dispose called"
+
+        client = self.create_client()
+        client.logout(self.session_id)
+        self.session_id = None
+        return "Ok"
+
+
 if __name__ == '__main__':
     import getpass
 
@@ -150,12 +239,12 @@ if __name__ == '__main__':
 
     admin_username = raw_input("admin username:")
     admin_password = getpass.getpass("admin password:")
+    username = raw_input("Dime un nuevo usuario")
+    password = raw_input("Dime una nueva password")
 
     client = UnedMicroClient("62.204.201.32:90", admin_username, admin_password)
 
-    client.register_student("porduna2", "porduna2", "porduna2")
-    username = "porduna2"
-    password = "porduna2"
+    client.register_student(username, username, password)
     session_id = client.login(username, password)
     print client.is_still_logged(session_id)
     print client.create_url(session_id)
