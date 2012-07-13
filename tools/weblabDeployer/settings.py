dd8476c4fccddd1d20642193602394ed9cede4c4
Xabier Larrakoetxea <slok69@gmail.com>
2012-07-13 11:39:06 +0200

    Added settings

---
+++ tools/weblabDeployer/settings.py
@@ -0,0 +1,15 @@
+
+#Flask configuration
+DEBUG = True
+SECRET_KEY = 'development key'
+
+# DB configuration
+#Connection String postgresql://DB_USERNAME:DB_PASSWORD@DB_HOST:DB_PORT/DB_NAME'
+
+
+DB_NAME = 'weblab'
+DB_HOST = ''
+DB_PORT = 5432
+DB_USERNAME = 'postgres'
+DB_PASSWORD = 'postgres'
+
