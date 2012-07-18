202b7962ff6f8392b589b60a308009d08ac5a56f
Xabier Larrakoetxea <slok69@gmail.com>
2012-07-18 11:20:07 +0200

    Added bootstrap 2.0.4 resources

--- tools/weblabDeployer/weblabDeployer/views.py
+++ tools/weblabDeployer/weblabDeployer/views.py
@@ -2,5 +2,4 @@ from weblabDeployer import app
 
 @app.route('/')
 def index():
-    raise Exception('Hi I\'m an exception!')
-    return 'This is not the funciton that you want'
+    return render_template('base.html')
