e441750c670eb88da11a3ac22fd370c7a3074885
Xabier Larrakoetxea <slok69@gmail.com>
2012-07-23 11:55:42 +0200

    Added licenses

--- tools/weblabDeployer/weblabDeployer/forms.py
+++ tools/weblabDeployer/weblabDeployer/forms.py
@@ -1,3 +1,22 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2012 onwards University of Deusto
+# All rights reserved.
+#
+# This software is licensed as described in the file COPYING, which
+# you should have received as part of this distribution.
+#
+# This software consists of contributions made by many individuals,
+# listed below:
+#
+# Author: Xabier Larrakoetxea <xabier.larrakoetxea@deusto.es>
+# Author: Pablo Orduña <pablo.orduna@deusto.es>
+#
+# These authors would like to acknowledge the Spanish Ministry of science
+# and innovation, for the support in the project IPT-2011-1558-430000
+# "mCloud: http://innovacion.grupogesfor.com/web/mcloud"
+#
+
 from wtforms import Form, BooleanField, TextField, PasswordField, validators
 
 class LoginForm(Form):
