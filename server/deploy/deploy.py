1d1a34d15ba5c59f3fe86ddd6fc27769371110f1
Pablo Orduña <pablo.orduna@deusto.es>
2012-08-02 11:36:23 +0200

    Support ZSI stub generation at setup.py

--- server/deploy/deploy.py
+++ server/deploy/deploy.py
@@ -103,63 +103,13 @@ def deployer(order, activate = True):
         return d
     return wrapped
 
-
 # Deployment functions
 
-@deployer(10)
-def check_installed_python_modules(error_handler):
-    """
-    Check for installed Python modules first:
-
-        * python-mysqldb or (python-pymysql with pymysql_sa)
-        * python-sqlalchemy
-        * python-simplejson [not required in Python >= 2.6]
-        * python-soappy  [optional]
-        * python-zsi     [optional]
-        * python-ldap    [optional]
-        * python-lxml    [optional]
-        * python-serial  [optional]
-        * mocker         [optional]
-    """
-    REQUIRED_MODULES = ['sqlalchemy'] # + (MySQLdb or (pymysql + pymysql_sa))
-    OPTIONAL_MODULES = ('ZSI','SOAPpy','lxml','mocker','serial','ldap')
-
-    not_installed_modules = []
-
-    def try_module(module_name, level):
-        try:
-            __import__(module_name)
-        except ImportError:
-            not_installed_modules.append((module_name, level))
-    
-    Required = 'Required'
-    Optional = 'Optional'
-
-    try_module('MySQLdb', Required)
-    if not_installed_modules:
-        not_installed_modules = []
-        try_module('pymysql', Required)
-        try_module('pymysql_sa', Required)
-        if not_installed_modules: # Add it again so as to document it
-            try_module('MySQLdb', Required)
-            
-
-    for i in REQUIRED_MODULES:
-        try_module(i, Required)
-    for i in OPTIONAL_MODULES:
-        try_module(i, Optional)
-    
-    failed = False
-    for i in not_installed_modules:
-        if i[1] == Required:
-            failed = True
-            error_handler.write_error("Required module %s not installed" % i[0])
-        else:
-            error_handler.write_warning("Optional module %s not installed" % i[0])
-
-    return failed
-
-def _wait_process(pr, error_handler):
+#@deployer(20)
+def create_databases(error_handler):
+    cwd = os.getcwd()
+    os.chdir('../deploy')
+    pr = subprocess.Popen(sys.executable + " create_databases.py", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     result = pr.wait()
     if result != 0:
         for i in pr.stdout.read().split('\n'):
@@ -167,103 +117,16 @@ def _wait_process(pr, error_handler):
         for i in pr.stderr.read().split('\n'):
             error_handler.write_error('stderr::%s' % i)
         error_handler.write_error("database script failed")
-        return True
-    if VERY_VERBOSE:
-        for i in pr.stdout.read().split('\n'):
-            error_handler.write_msg('stdout::%s' % i)
-        for i in pr.stderr.read().split('\n'):
-            error_handler.write_warning('stderr::%s' % i)
-    return False
-
-
-@deployer(20)
-def create_databases(error_handler):
-    cwd = os.getcwd()
-    os.chdir('../deploy')
-    pr = subprocess.Popen(sys.executable + " create_databases.py", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    result = _wait_process(pr, error_handler)
+        result = True
+    else:
+        if VERY_VERBOSE:
+            for i in pr.stdout.read().split('\n'):
+                error_handler.write_msg('stdout::%s' % i)
+            for i in pr.stderr.read().split('\n'):
+                error_handler.write_warning('stderr::%s' % i)
+        result = False
     os.chdir(cwd)
     return result
-
-
-def _deploy_stubs(error_handler, folder, wsdl_file, filename):
-    try:
-        import ZSI
-    except ImportError:
-        error_handler.write_error("Skipping stubs and skeletons deployment; ZSI not installed")
-        return False
-        
-    bin_path      = os.path.abspath('../lib/common/bin/')
-    cur_dir_full  = os.path.abspath(os.path.curdir)
-    if bin_path.find(cur_dir_full) == 0:
-        bin_path = '.' + bin_path[len(cur_dir_full):]
-    wsdl2py       = '"%s"' % os.sep.join((bin_path,'wsdl2py.py'))
-    wsdl2dispatch = '"%s"' % os.sep.join((bin_path,'wsdl2dispatch.py'))
-
-    for i in glob.glob("%s/%s_*.py" % (folder, filename)):
-        os.remove(i)
-    cwd = os.getcwd()
-    os.chdir(folder)
-
-    pr = subprocess.Popen(sys.executable + " " + wsdl2py + " -e -f ../%s --simple-naming" % wsdl_file, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    result = pr.wait()
-    if result != 0:
-        for i in pr.stdout.read().split('\n'):
-            if i.strip() != '':
-                error_handler.write_msg('stdout::%s' % i)
-        for i in pr.stderr.read().split('\n'):
-            if i.strip() != '':
-                error_handler.write_error('stderr::%s' % i)
-        error_handler.write_error("wsdl2py script failed")
-        os.chdir(cwd)
-        return True
-    if VERY_VERBOSE:
-        for i in pr.stdout.read().split('\n'):
-            if i.strip() != '':
-                error_handler.write_msg('stdout::%s' % i)
-        for i in pr.stderr.read().split('\n'):
-            if i.strip() != '' and i.find("DeprecationWarning: the multifile module has been deprecated") < 0 and i.find("import multifile, mimetools") < 0:
-                error_handler.write_warning('stderr::%s' % i)
-
-    # Little bug in the stubs generation :-)
-    client_file = open('%s_client.py' % filename,'a')
-    client_file.write('\n\nfrom %s_messages import *\n\n' % filename)
-    client_file.close()
-
-    # By default, ZSI strips the strings in the SOAP request. We don't want this, so we change this behaviour
-    services_types_content = open('%s_services_types.py' % filename).read()
-    services_types_content = services_types_content.replace("ZSI.TC.String(","ZSI.TC.String(strip=False,")
-    open('%s_services_types.py' % filename,'w').write(services_types_content)
-
-    pr = subprocess.Popen(sys.executable + " " + wsdl2dispatch + " -e -f ../%s --simple-naming" % wsdl_file, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    result = pr.wait()
-    os.chdir(cwd)
-    if result != 0:
-        for i in pr.stdout.read().split('\n'):
-            if i.strip() != '':
-                error_handler.write_msg('stdout::%s' % i)
-        for i in pr.stderr.read().split('\n'):
-            if i.strip() != '':
-                error_handler.write_error('stderr::%s' % i)
-        error_handler.write_error("wsdl2dispatch script failed")
-        return True
-    if VERY_VERBOSE:
-        for i in pr.stdout.read().split('\n'):
-            if i.strip() != '':
-                error_handler.write_msg('stdout::%s' % i)
-        for i in pr.stderr.read().split('\n'):
-            if i.strip() != '' and i.find("DeprecationWarning: the multifile module has been deprecated") < 0 and i.find("import multifile, mimetools") < 0:
-                error_handler.write_warning('stderr::%s' % i)
-    
-    return False
-
-@deployer(30)
-def deploy_login_stubs(error_handler):
-    return _deploy_stubs(error_handler, 'weblab/login/comm/generated/', 'LoginWebLabDeusto.wsdl', 'loginweblabdeusto')
-
-@deployer(30)
-def deploy_core_stubs(error_handler):
-    return _deploy_stubs(error_handler, 'weblab/core/comm/generated/', 'UserProcessingWebLabDeusto.wsdl', 'weblabdeusto')
 
 @deployer(40)
 def build_webserver(error_handler):
--- server/src/setup.py
+++ server/src/setup.py
@@ -14,6 +14,8 @@
 #
 
 import os
+import sys
+import subprocess
 import glob
 import shutil
 from setuptools import setup
@@ -32,6 +34,90 @@ def fullsplit(path, result=None):
     if head == path:
         return result
     return fullsplit(head, [tail] + result)
+
+##########################################################
+# 
+# 
+#       S O A P     S T U B S  ( O P T I O N A L )
+# 
+# 
+
+def deploy_stubs(folder, wsdl_file, filename):        
+    wsdl2py       = 'wsdl2py'
+    wsdl2dispatch = 'wsdl2dispatch'
+
+    for i in glob.glob("%s/%s_*.py" % (folder, filename)):
+        os.remove(i)
+    cwd = os.getcwd()
+    os.chdir(folder)
+
+    pr = subprocess.Popen(wsdl2py + " -e -f %s --simple-naming" % wsdl_file, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    result = pr.wait()
+    if result != 0:
+        for i in pr.stdout.read().split('\n'):
+            if i.strip() != '':
+                print 'INFO: stdout::%s' % i
+        for i in pr.stderr.read().split('\n'):
+            if i.strip() != '':
+                print >> sys.stderr, 'ERR: stderr::%s' % i
+        print >> sys.stderr, "ERR: wsdl2py script failed"
+        os.chdir(cwd)
+        return True
+    if VERY_VERBOSE:
+        for i in pr.stdout.read().split('\n'):
+            if i.strip() != '':
+                print 'INFO: stdout::%s' % i
+        for i in pr.stderr.read().split('\n'):
+            if i.strip() != '' and i.find("DeprecationWarning: the multifile module has been deprecated") < 0 and i.find("import multifile, mimetools") < 0:
+                print >> sys.stderr, 'WARN: stderr::%s' % i
+
+    # Little bug in the stubs generation :-)
+    client_file = open('%s_client.py' % filename,'a')
+    client_file.write('\n\nfrom %s_messages import *\n\n' % filename)
+    client_file.close()
+
+    # By default, ZSI strips the strings in the SOAP request. We don't want this, so we change this behaviour
+    services_types_content = open('%s_services_types.py' % filename).read()
+    services_types_content = services_types_content.replace("ZSI.TC.String(","ZSI.TC.String(strip=False,")
+    open('%s_services_types.py' % filename,'w').write(services_types_content)
+
+    pr = subprocess.Popen(wsdl2dispatch + " -e -f %s --simple-naming" % wsdl_file, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    result = pr.wait()
+    os.chdir(cwd)
+    if result != 0:
+        for i in pr.stdout.read().split('\n'):
+            if i.strip() != '':
+                print 'INFO: stdout::%s' % i
+        for i in pr.stderr.read().split('\n'):
+            if i.strip() != '':
+                print >> sys.stderr, 'ERR: stderr::%s' % i
+        print >> sys.stderr, "ERR: wsdl2dispatch script failed"
+        return True
+    if VERY_VERBOSE:
+        for i in pr.stdout.read().split('\n'):
+            if i.strip() != '':
+                print 'INFO: stdout::%s' % i
+        for i in pr.stderr.read().split('\n'):
+            if i.strip() != '' and i.find("DeprecationWarning: the multifile module has been deprecated") < 0 and i.find("import multifile, mimetools") < 0:
+                print 'WARN: stderr::%s' % i
+    
+    return False
+
+try:
+    import ZSI
+except ImportError:
+    print >> sys.stderr, "Skipping stubs and skeletons deployment; ZSI not installed"
+else:
+    deploy_stubs(os.path.join('weblab', 'login', 'comm', 'generated'), os.path.join('..', 'LoginWebLabDeusto.wsdl'), 'loginweblabdeusto')
+    deploy_stubs(os.path.join('weblab','core','comm','generated'), os.path.join('..', 'UserProcessingWebLabDeusto.wsdl'), 'weblabdeusto')
+
+
+##########################################################
+#
+# 
+#       P A C K A G E S 
+# 
+# 
 
 if not os.path.exists('weblabdeusto_data'):
     os.mkdir('weblabdeusto_data')
@@ -60,7 +146,14 @@ for weblab_dir in ['voodoo','weblab','experiments']:
                     os.remove(os.path.join(new_path, f))
                 shutil.copy2(os.path.join(dirpath, f), os.path.join(new_path, f))
 
-if len(glob.glob("../../client/war/weblabclient/*.html")) > 0:
+##########################################################
+#
+# 
+#       C L I E N T 
+# 
+# 
+
+if len(glob.glob("../../client/war/weblabclientlab/*.html")) > 0:
     # The client has been compiled. Let's copy it here.
     shutil.rmtree("weblabdeusto_data/war", True)
     shutil.copytree("../../client/war/","weblabdeusto_data/war")
@@ -78,6 +171,34 @@ for dirpath, dirnames, filenames in os.walk('weblabdeusto_data'):
 
     data_files.append([dirpath, [os.path.join(dirpath, f) for f in filenames]])
 
+##########################################################
+#
+# 
+#       R E Q U I R E M E N T S
+# 
+# 
+def load_requires(dest, filename):
+    for line in open(filename):
+        if line.find('#') >= 0:
+            package_name = line[:line.find('#')]
+        else:
+            package_name = line
+        if package_name.strip() != '':
+            dest.append(package_name)
+
+install_requires = []
+tests_require    = []
+
+load_requires(install_requires, 'requirements.txt')
+load_requires(install_requires, 'requirements_recommended.txt')
+load_requires(tests_require,    'requirements_testing.txt')
+
+##########################################################
+#
+# 
+#       G E N E R A L
+# 
+# 
 
 scripts = [ 'weblab/admin/weblab-admin.py' ]
 
@@ -94,22 +215,6 @@ classifiers=[
 ]
 
 cp_license="BSD"
-
-def load_requires(dest, filename):
-    for line in open(filename):
-        if line.find('#') >= 0:
-            package_name = line[:line.find('#')]
-        else:
-            package_name = line
-        if package_name.strip() != '':
-            dest.append(package_name)
-
-install_requires = []
-tests_require    = []
-
-load_requires(install_requires, 'requirements.txt')
-load_requires(install_requires, 'requirements_recommended.txt')
-load_requires(tests_require,    'requirements_testing.txt')
 
 setup(name='weblabdeusto',
       version='4.0',
