e0417ac5d461c614f06238d5b5657fc74c8b6323
Luis Rodriguez <luis.rodriguez@opendeusto.es>
2012-09-25 12:27:44 +0200

    version.py was not working in Microsoft Windows since the 'git' process does not exist (git.cmd does)

--- client/version.py
+++ client/version.py
@@ -4,36 +4,38 @@ import sys
 import time
 import subprocess
 import optparse
+import shlex
+
+try:
+    subprocess.check_output(shlex.split('git --help'))
+except:
+    try:
+        subprocess.check_output(shlex.split('git.cmd --help'))
+    except:
+        print >> sys.stderr, "git command could not be run! Check your PATH"
+        sys.exit(-1)
+    else:
+        git_command = 'git.cmd'
+else:
+    git_command = 'git'
+
 
 def get_version():
-    cmd = ['git','--no-pager','show']
-    p = subprocess.Popen(cmd, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
-    result = p.wait()
-    if result != 0:
-        print >> sys.stderr, "Error executing %s" % ' '.join(cmd)
-        return None
-    return p.stdout.readlines()[0].split(' ')[1].strip()
+    output = subprocess.check_output([git_command,'--no-pager','show'])
+    return output.split('\n')[0].split()[1].strip()
 
 def get_number_of_versions(version):
     # git log 9bfcfb14afefd80473d4028c24f6b5019ebc3a5b --format="%h"
-    cmd = ['git','--no-pager','log', version, '--format="%at"']
-    p = subprocess.Popen(cmd, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
-    result = p.wait()
-    if result != 0:
-        print >> sys.stderr, "Error executing %s" % ' '.join(cmd)
-        return None
-    lines = [ line for line in p.stdout.readlines() ]
+    cmd = shlex.split('%s --no-pager log %s --format="%%at"' % (git_command, version))
+    output = subprocess.check_output(cmd)
+    lines = [ line for line in output.split('\n') ]
     timestamp = int(lines[0].strip().replace('"','').replace("'",''))
     date_str = time.strftime('%A, %B %d, %Y', time.localtime(timestamp))
     return len(lines), date_str
 
 def generate(filename):
     version = get_version()
-    if version is None:
-        return
     number, date = get_number_of_versions(version)
-    if number is None:
-        return
     msg = r"""var wlVersionMessage = "WebLab-Deusto r<a href=\"https://github.com/porduna/weblabdeusto/commits/%(version)s\">%(version_number)s</a> | Last update: %(date)s";""" % {
         'version'         : version,
         'version_number'  : number,
