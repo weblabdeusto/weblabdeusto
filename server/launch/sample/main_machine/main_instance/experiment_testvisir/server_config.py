dec2692ad54c7638ea3cb763fc8c04a95f84a3fe
Pablo Orduña <pablo.orduna@deusto.es>
2013-02-11 12:14:05 +0100

    Fix savedata in VISIR

--- server/launch/sample/main_machine/main_instance/experiment_testvisir/server_config.py
+++ server/launch/sample/main_machine/main_instance/experiment_testvisir/server_config.py
@@ -19,19 +19,15 @@ vt_debug_prints = False
 # 
 # vt_teacher = False
 # vt_not_savedata = \
-"""
+vt_savedata = """
 <save>
-	<instruments+list="breadboard/breadboard.swf|multimeter/multimeter.swf|functiongenerator/functiongenerator.swf|oscilloscope/oscilloscope.swf|tripledc/tripledc.swf"+/>
-	<multimeter+/>
+	<instruments list="breadboard/breadboard.swf|multimeter/multimeter.swf|functiongenerator/functiongenerator.swf|oscilloscope/oscilloscope.swf|tripledc/tripledc.swf"/>
+	<multimeter/>
 		<circuit>
 			<circuitlist>
-				<component>R+1.6k+52+26+0</component>
-				<component>R+2.7k+117+26+0</component>
-				<component>R+10k+182+78+0</component>
-				<component>R+10k+182+52+0</component>
-				<component>R+10k+182+26+0</component>
-				<component>C+56n+247+39+0</component>
-				<component>C+56n+247+91+0</component>
+              <component>R 10k 65 13 0</component>
+              <component>R 10k 65 39 0</component>
+              <component>R 1k 65 65 0</component>
 			</circuitlist>
 		</circuit>
 </save>
