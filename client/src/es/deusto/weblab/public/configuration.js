800721df9bd0ef75b276dc979c91db74ebe542c3
Pablo Orduña <pablo.orduna@deusto.es>
2013-02-12 17:02:29 +0100

    Add Fisica-{1,2,3}-PXI

--- client/src/es/deusto/weblab/public/configuration.js
+++ client/src/es/deusto/weblab/public/configuration.js
@@ -300,6 +300,27 @@
 	                        	   "experiment.info.description" : "description"
 	                           },
 	                           {
+	                        	   "experiment.name"      : "Fisica-1-PXI",
+	                        	   "experiment.category"  : "Visir experiments",
+	                        	   "experiment.picture"   : "/img/experiments/visir.jpg",
+	                           	   "experiment.info.link" : "http://weblabdeusto.readthedocs.org/en/latest/sample_labs.html#visir",
+	                        	   "experiment.info.description" : "description"
+	                           },
+	                           {
+	                        	   "experiment.name"      : "Fisica-2-PXI",
+	                        	   "experiment.category"  : "Visir experiments",
+	                        	   "experiment.picture"   : "/img/experiments/visir.jpg",
+	                           	   "experiment.info.link" : "http://weblabdeusto.readthedocs.org/en/latest/sample_labs.html#visir",
+	                        	   "experiment.info.description" : "description"
+	                           },
+	                           {
+	                        	   "experiment.name"      : "Fisica-3-PXI",
+	                        	   "experiment.category"  : "Visir experiments",
+	                        	   "experiment.picture"   : "/img/experiments/visir.jpg",
+	                           	   "experiment.info.link" : "http://weblabdeusto.readthedocs.org/en/latest/sample_labs.html#visir",
+	                        	   "experiment.info.description" : "description"
+	                           },
+	                           {
 	                        	   "experiment.name"      : "visirtest",
 	                        	   "experiment.category"  : "Dummy experiments",
 	                        	   "experiment.picture"   : "/img/experiments/visir.jpg",
