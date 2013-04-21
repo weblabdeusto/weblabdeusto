7cd74b37f0f2be43a6b56f0898f68ef166de3f87
Pablo Orduña <pablo.orduna@deusto.es>
2013-04-21 19:18:24 +0200

    Add initial upgrade for supporting exp_info

---
+++ server/src/weblab/db/upgrade/scheduling/versions/2ecc7c4ec0c5_add_experiment_infor.py
@@ -0,0 +1,22 @@
+"""Add experiment information
+
+Revision ID: 2ecc7c4ec0c5
+Revises: None
+Create Date: 2013-04-21 19:16:09.441855
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '2ecc7c4ec0c5'
+down_revision = None
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
