ccca1b84472a01d6f792fe9af4a81e454975f09f
Luis Rodriguez <luis.rodriguezgil@deusto.es>
2013-10-04 13:45:12 +0200

    [xilinxc]: Updated clean script

--- server/src/experiments/xilinxc/files_cpld/clean.bat
+++ server/src/experiments/xilinxc/files_cpld/clean.bat
@@ -1,2 +1,24 @@
 del *.xrpt
 RD /S /Q _xmsgs
+RD /S /Q _ngo
+RD /S /Q cpld_weblab_html
+RD /S /Q xlnx_auto_0_xdb
+RD /S /Q "xst/dump.xst"
+RD /S /Q "xst/work"
+del *.bld
+del *.gyd
+del *.jed
+del *.lso
+del *.mfd
+del *.ngd
+del *.ngc
+del *.ngr
+del *.pad
+del *.pnx
+del *.rpt
+del *.prj
+del *.err
+del *.csv
+del *.xml
+del *.syr
+del *.vm6
