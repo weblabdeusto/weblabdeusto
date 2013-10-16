28cbcc3e5c1ba080ec6f99562cbb63e90d94d880
Luis Rodriguez <luis.rodriguezgil@deusto.es>
2013-10-16 11:58:08 +0200

    [xilinxc]: Fix

--- server/src/experiments/xilinxc/files_cpld/build.sh
+++ server/src/experiments/xilinxc/files_cpld/build.sh
@@ -2,10 +2,10 @@
 # Force an exit if any command returns non-zero (error code).
 set -e 
 
-xst -intstyle ise -ifn weblab.xst -ofn weblab.syr
+xst -intstyle ise -ifn base.xst -ofn base.syr
 
-ngdbuild -intstyle ise -dd _ngo -uc weblab_res.ucf -p xc9572-PC84-7 weblab.ngc weblab.ngd
+ngdbuild -intstyle ise -dd _ngo -uc cpld_weblab_res.ucf -p xc9572-PC84-7 base.ngc base.ngd
 
-cpldfit -intstyle ise -p xc9572-7-PC84 -ofmt vhdl -optimize speed -htmlrpt -loc on -slew fast -init low -inputs 36 -pterms 25 -power std -localfbk -pinfbk weblab.ngd
+cpldfit -intstyle ise -p xc9572-7-PC84 -ofmt vhdl -optimize speed -htmlrpt -loc on -slew fast -init low -inputs 36 -pterms 25 -power std -localfbk -pinfbk base.ngd
 
-hprep6 -s IEEE1149 -n weblab -i weblab
+hprep6 -s IEEE1149 -n base -i base
