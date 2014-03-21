7ad2f9df7210387f6510fbd637296428f3d25828
Razican <admin@razican.com>
2014-03-21 15:14:51 +0100

    Started with answers

--- client/src/es/deusto/weblab/public/romie/js/game.js
+++ client/src/es/deusto/weblab/public/romie/js/game.js
@@ -1,10 +1,33 @@
-showQuestion = function(question)
+Game = function(romie)
+{
+	this.romie = romie;
+}
+
+Game.prototype.showQuestion = function(question)
 {
 	$('#questionLabel').html(question["question"]);
 
+	i = 0;
 	question["answers"].forEach(function(answer)
 	{
-		$('#question .modal-body ul').append('<li>'+answer+"</li>");
-	})
+		$('#question .modal-body form')
+			.append('<input type="radio" name="answer" id="ans_'+i+'" value="'+i+'">'+
+				'<label for="ans_'+i+'">'+answer+'</label><br>');
+		i++;
+	}.bind(i));
 	$("#question").modal({keyboard:false});
+
+	this.question = question;
+}
+
+Game.prototype.answerQuestion = function()
+{
+	answer = parseInt($('#question input[name="answer"]:checked').val());
+	console.log(answer);
+	// Get answer
+	// Send command
+	//    if correct -> show dialog + add points & movements
+	//    if incorrect -> show dialog
+	// Hide question
+	// clear question
 }
