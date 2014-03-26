5a1474500d86d21a0f48d28d264277fb4ca41c90
Luis Rodriguez-Gil <luis.rodriguezgil@deusto.es>
2014-03-26 15:59:55 +0100

    [purejs]: Work into the purejs js lib

--- client/war/purejs/purejs.js
+++ client/war/purejs/purejs.js
@@ -16,9 +16,32 @@
 })(jQuery);
 
 
+
+
+///////////////////////////////////////////////////////////////
+//
+// WEBLAB PSEUDO-CLASS
+// The following class-like function provides the JavaScript
+// API that Weblab provides to enable simple interaction for
+// its experiments. The interface relies heavily on callbacks,
+// to receive the events that the experiment requires to function,
+// including but not limited to interaction start notifications,
+// message responses, etc.
+//
+///////////////////////////////////////////////////////////////
 Weblab = new function()
 {
-    var mTargetUrl = "http://localhost/weblab/json/";
+
+    ///////////////////////////////////////////////////////////////
+    //
+    // PRIVATE ATTRIBUTES AND FUNCTIONS
+    // The API uses these internally to provide an easier to use,
+    // higher level API. Users of this class do not need to be
+    // aware of them.
+    //
+    ///////////////////////////////////////////////////////////////
+
+    var mTargetURL;
     var mReservation;
 
     //! Extracts the reservation id from the URL (in the hash).
@@ -28,17 +51,35 @@ Weblab = new function()
         mReservation = $.QueryString["reservation"];
     };
 
+    //! Extracts the targeturl from the URL (in the hash). This is the URL towards which
+    //! the AJAX requests will be directed. If not specified through the URL, then
+    //! it will use a default (<location>/weblab/json/).
+    this._extractTargetURL = function() {
+        mTargetURL = $.QueryString["targeturl"];
+        if(mTargetURL == undefined)
+            mTargetURL = document.location.origin + "/weblab/json/";
+    };
+
+
     //! Gets the reservation that Weblab is using.
     //!
     //! @return The reservation ID.
-    this.getReservation = function() {
+    this._getReservation = function() {
         return mReservation;
     };
+
+    //! Gets the TargetURL that we are using.
+    //!
+    //! @return The target URL.
+    this._getTargetURL = function() {
+        return mTargetURL;
+    };
+
 
     this._poll = function() {
         var request = {"method": "poll", "params": {"reservation_id": {"id": mReservation}}};
 
-        $.post(mTargetUrl, JSON.stringify(request), function(success) {
+        $.post(mTargetURL, JSON.stringify(request), function(success) {
                 console.log("Data received: " + success);
                 console.log(success);
             }, "json"
@@ -48,19 +89,22 @@ Weblab = new function()
     this._get_reservation_status = function() {
         var request = {"method": "get_reservation_status", "params": {"reservation_id": {"id": mReservation}}};
 
-        $.post(mTargetUrl, JSON.stringify(request), function(success) {
+        $.post(mTargetURL, JSON.stringify(request), function(success) {
                 console.log("Data received: " + success);
                 console.log(success);
             }, "json"
         );
     };
 
-    this._send_command = function(command) {
+    this._send_command = function(command, success, error) {
         var request = {"method": "send_command", "params": {"command": {"commandstring": command}, "reservation_id": {"id": mReservation}}};
 
-        $.post(mTargetUrl, JSON.stringify(request), function(success) {
-                console.log("Data received: " + success);
-                console.log(success);
+        $.post(mTargetURL, JSON.stringify(request), function(success_data) {
+                console.log("Data received: " + success_data);
+                console.log(success_data);
+
+                if(success != undefined)
+                    success(success_data);
             }, "json"
         );
     };
@@ -68,7 +112,7 @@ Weblab = new function()
     this._finished_experiment = function(command) {
         var request = {"method": "finished_experiment", "params": {"reservation_id": {"id": mReservation}}};
 
-        $.post(mTargetUrl, JSON.stringify(request), function(success) {
+        $.post(mTargetURL, JSON.stringify(request), function(success) {
                 console.log("Data received: " + success);
                 console.log(success);
             }, "json"
@@ -76,8 +120,61 @@ Weblab = new function()
     }
 
 
+
+    ///////////////////////////////////////////////////////////////
+    //
+    // PUBLIC INTERFACE
+    // The following methods are part of the public interface of this
+    // class. They can be used freely. Several of them rely on callbacks.
+    // They might not work properly if they are run stand-alone, on a
+    // context different than Weblab-Deusto.
+    //
+    ///////////////////////////////////////////////////////////////
+
+
+    //! Sends a command to the experiment server.
+    //!
+    //! @param text Text of the command.
+    //! @param successHandler Callback that will receive the response for the command.
+    //! Takes a single string as argument.
+    //! @param errorHandler Callback that will receive the response for the command.
+    //! Takes a single string as argument.
+    //!
+    this.sendCommand = new function(command, successHandler, errorHandler) {
+        this.sendCommand(command, successHandler, errorHandler);
+    };
+
+
+    //! Sends a command to the experiment server and prints the result to console.
+    //! If the command was successful it is printed to the stdout and otherwise to stderr.
+    //!
+    //! @param text: Command to send.
+    this.testCommand = new function(command) {
+        this.sendCommand(command, function(success) {
+            console.log("SUCCESS: " + success);
+        }, function(error) {
+            console.error("ERROR: " + error);
+        });
+    };
+
+
+
+    ///////////////////////////////////////////////////////////////
+    //
+    // CONSTRUCTOR
+    // The following is internal code to create the object.
+    //
+    ///////////////////////////////////////////////////////////////
+
+    // Extract the reservation id from the hash.
     this._extractReservation();
-};
+
+    // Extract the target URL from the hash or set a default one.
+    this._extractTargetURL();
+
+    //$.cookie("weblabsessionid", "T-Go5baSwSB3SHsO.route2");
+
+}; // !Weblab
 
 
 
