26b4b2f5d30c237eceba82a8eb473d02b7029ddb
Luis Rodriguez-Gil <luis.rodriguezgil@deusto.es>
2014-10-14 14:07:27 +0200

    [newclient]: Added testweblab_launcher script by Pablo

---
+++ flask_client/testpjs/testweblab_launcher.py
@@ -0,0 +1,36 @@
+import os
+import threading
+import sys
+
+original_path = os.path.abspath(os.curdir)
+
+weblab_path = "../../server/launch/sample"
+os.chdir(weblab_path)
+sys.path.append(".")
+
+from launch_sample import inner
+
+condition = threading.Condition()
+event_notifier = threading.Condition()
+
+class Runner(threading.Thread):
+    def run(self):
+        self.launcher = inner(condition = condition, event_notifier = event_notifier)
+        self.launcher.launch()
+
+runner = Runner()
+runner.start()
+
+with event_notifier:
+    event_notifier.wait()
+
+os.chdir(original_path)
+
+result = os.system("mocha-phantomjs -s web-security=no -s localToRemoteUrlAccessEnabled=true -s webSecurityEnabled=false testrunner.html")
+
+os.chdir(weblab_path)
+
+with condition:
+    condition.notify()
+
+sys.exit(result)
