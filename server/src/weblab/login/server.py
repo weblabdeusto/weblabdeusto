e3ba71a4b2b4441ed6eeda72d9ceb497992f0e5d
Pablo Orduña <pablo.orduna@deusto.es>
2014-12-29 02:53:13 +0100

    weblab.login can now be removed

--- server/src/weblab/login/server.py
+++ server/src/weblab/login/server.py
@@ -13,54 +13,13 @@
 # Author: Pablo Orduña <pablo@ordunya.com>
 #
 
-"""
-This module contains the Login Server. This server will only
-manage the authentication (and not the authorization) of the 
-users. To this end, it defines that there are two interfaces:
-
- * One for those systems that can be checked with typical credentials (e.g.,
-   using a username a password). Examples of this would be a regular database,
-   a LDAP server, or a set of constraints such as "is the user coming from 
-   this IP address?". This interface is called "SimpleAuthn".
-
- * The system, which manages that there is an external system that manages
-   the authentication, and some external protocol is required. Examples of this
-   are OAuth, OpenID or similar. They all require using a web interface. This
-   interface is called "WebProtocolAuthn".
-
-The LoginServer manages both interfaces.
-"""
-
-import voodoo.log as log
-
 # TODO list:
 # - Remove this file completely
 
 
 class LoginServer(object):
-
-    FACADE_SERVERS = () #  wsgi_server.LoginWsgiRemoteFacadeServer, )
-
     def __init__(self, coord_address, locator, cfg_manager, dont_start = False, *args, **kwargs):
         super(LoginServer,self).__init__(*args, **kwargs)
 
-        log.log( LoginServer, log.level.Info, "Starting Login Server")
-
-        self._locator       = locator
-        self._cfg_manager   = cfg_manager
-
-        self._facade_servers       = []
-
-        if not dont_start:
-            for ServerClass in self.FACADE_SERVERS:
-                self._facade_servers.append(ServerClass( self, cfg_manager ))
-
-            for server in self._facade_servers:
-                server.start()
-
     def stop(self):
-        if hasattr(super(LoginServer, self), 'stop'):
-            super(LoginServer, self).stop()
-        for server in self._facade_servers:
-            server.stop()
-
+        pass
