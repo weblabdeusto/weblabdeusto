f34b89d1c707b0dfefb35e29c2fd13eea62abc0b
Pablo Orduña <pablo.orduna@deusto.es>
2015-01-19 01:04:26 +0100

    Add sample clients to gen

--- server/src/voodoo/gen2/__init__.py
+++ server/src/voodoo/gen2/__init__.py
@@ -1,4 +1,6 @@
 import re
+from abc import ABCMeta, abstractmethod
+
 import yaml
 
 from voodoo.gen2.exc import GeneratorException
@@ -6,6 +8,11 @@ from voodoo.gen2.exc import GeneratorException
 LAB_CLASS  = 'weblab.lab.server.LaboratoryServer'
 CORE_CLASS = 'weblab.core.server.UserProcessingServer'
 PROTOCOL_PRIORITIES = ('http', 'xmlrpc')
+
+############################################
+# 
+#   Configuration structures
+# 
 
 class GlobalConfig(dict):
     def __init__(self, config_files, config_values):
@@ -49,6 +56,84 @@ class ProtocolsConfig(dict):
     def __init__(self, port):
         super(ProtocolsConfig, self).__init__()
         self.port = port
+
+
+############################################
+# 
+#   Configuration loading
+# 
+
+def _process_config(tree):
+    config_files = []
+    config_values = {}
+
+    if 'config_files' in tree:
+        config_files.extend(tree['config_files'])
+
+    if 'config_file' in tree:
+        config_files.append(tree['config_file'])
+
+    if 'config' in tree:
+        config_values.update(tree['config'])
+
+    return config_files, config_values
+
+def load(yaml_file):
+    return _load_contents(yaml.load(open(yaml_file)))
+
+def loads(yaml_file):
+    return _load_contents(yaml.loads(yaml_contents))
+
+def _load_contents(contents):
+    global_value = contents
+
+    config_files, config_values = _process_config(global_value)
+    global_config = GlobalConfig(config_files, config_values)
+
+    for host_name, host_value in global_value.get('hosts', {}).iteritems():
+        config_files, config_values = _process_config(host_value)
+        host = host_value.get('host')
+        host_config = HostConfig(config_files, config_values, host)
+        global_config[host_name] = host_config
+
+        for process_name, process_value in host_value.get('processes', {}).iteritems():
+            config_files, config_values = _process_config(process_value)
+            process_config = ProcessConfig(config_files, config_values)
+            host_config[process_name] = process_config
+        
+            for component_name, component_value in process_value.get('components', {}).iteritems():
+                config_files, config_values = _process_config(component_value)
+
+                # Type and class
+                component_type = component_value.get('type')
+                if not component_type:
+                    raise GeneratorException("Missing component type on %s:%s@%s" % (component_name, process_name, host_name))
+
+                if component_type == 'laboratory':
+                    component_class = LAB_CLASS
+                elif component_type == 'core':
+                    component_class = CORE_CLASS
+                else:
+                    component_class = component_value.get('class')
+                    if not component_class:
+                        raise GeneratorException("Missing component class on %s:%s@%s" % (component_name, process_name, host_name))
+
+                # Protocols
+                protocols = component_value.get('protocols', {})
+                port = protocols.pop('port', None)
+                protocols_config = ProtocolsConfig(port)
+                protocols_config.update(protocols)
+
+                component_config = ComponentConfig(config_files, config_values, component_type, component_class, protocols_config)
+                process_config[component_name] = component_config
+
+    return global_config
+
+
+############################################
+# 
+#   Locator
+# 
 
 class Locator(object):
     def __init__(self, global_config, my_coord_address):
@@ -119,6 +204,12 @@ class Locator(object):
                             addresses.append(external_component)
 
         return addresses
+
+
+############################################
+# 
+#   CoordAddress
+# 
 
 class CoordAddress(object):
 
@@ -195,69 +286,44 @@ class CoordAddress(object):
             component, process, host = m.groups()
             return CoordAddress(host,process,component)
 
-def _process_config(tree):
-    config_files = []
-    config_values = {}
 
-    if 'config_files' in tree:
-        config_files.extend(tree['config_files'])
+############################################
+# 
+#   Clients
+# 
 
-    if 'config_file' in tree:
-        config_files.append(tree['config_file'])
+class AbstractClient(object):
+    __metaclass__ = ABCMeta
 
-    if 'config' in tree:
-        config_values.update(tree['config'])
+    def __init__(self, server_type, server_config):
+        methods = [ 'foo', 'bar' ]
+        # Create methods in this instance for each of these methods
+        for method in methods:
+            def call_method(*args):
+                return self._call(method, *args)
+            setattr(self, method, call_method)
 
-    return config_files, config_values
+    @abstractmethod
+    def _call(self, name, *args):
+        """Call a method with the given name and arguments"""
 
-def load(yaml_file):
-    return _load_contents(yaml.load(open(yaml_file)))
+def create_client(server_type, args):
+    pass
 
-def loads(yaml_file):
-    return _load_contents(yaml.loads(yaml_contents))
+class DirectClient(AbstractClient):
+    def _call(self, name, *args):
+        print "Method: %s; args: %s" % (name,args)
+        # use voodoo.gen.registry
+        pass
 
-def _load_contents(contents):
-    global_value = contents
 
-    config_files, config_values = _process_config(global_value)
-    global_config = GlobalConfig(config_files, config_values)
+class HttpClient(AbstractClient):
+    def _call(self, name, *args):
+        # use requests and JSON or pickle (right now, pickle, in the future, json)
+        pass
 
-    for host_name, host_value in global_value.get('hosts', {}).iteritems():
-        config_files, config_values = _process_config(host_value)
-        host = host_value.get('host')
-        host_config = HostConfig(config_files, config_values, host)
-        global_config[host_name] = host_config
-
-        for process_name, process_value in host_value.get('processes', {}).iteritems():
-            config_files, config_values = _process_config(process_value)
-            process_config = ProcessConfig(config_files, config_values)
-            host_config[process_name] = process_config
-        
-            for component_name, component_value in process_value.get('components', {}).iteritems():
-                config_files, config_values = _process_config(component_value)
-
-                # Type and class
-                component_type = component_value.get('type')
-                if not component_type:
-                    raise GeneratorException("Missing component type on %s:%s@%s" % (component_name, process_name, host_name))
-
-                if component_type == 'laboratory':
-                    component_class = LAB_CLASS
-                elif component_type == 'core':
-                    component_class = CORE_CLASS
-                else:
-                    component_class = component_value.get('class')
-                    if not component_class:
-                        raise GeneratorException("Missing component class on %s:%s@%s" % (component_name, process_name, host_name))
-
-                # Protocols
-                protocols = component_value.get('protocols', {})
-                port = protocols.pop('port', None)
-                protocols_config = ProtocolsConfig(port)
-                protocols_config.update(protocols)
-
-                component_config = ComponentConfig(config_files, config_values, component_type, component_class, protocols_config)
-                process_config[component_name] = component_config
-
-    return global_config
+class XmlRpcClient(AbstractClient):
+    def _call(self, name, *args):
+        # use xmlprpclib
+        pass
 
