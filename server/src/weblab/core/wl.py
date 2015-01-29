6d0cb9b29c1600d1dfa3c86b716d532f5cbe112e
Luis Rodriguez-Gil <luis.rodriguezgil@deusto.es>
2015-01-29 18:35:05 +0100

    Minor changes

--- server/src/weblab/core/wl.py
+++ server/src/weblab/core/wl.py
@@ -2,4 +2,9 @@ from weblab.core.new_server import WebLabAPI
 
 weblab_api = WebLabAPI(['login_web', 'web', 'webclient'])
 
+@weblab_api.route_web("/test/", methods=["GET", "POST"])
+def test():
+    weblab_api.ctx.reservation_id = "TEST"
+    return "HAI"
+
 
