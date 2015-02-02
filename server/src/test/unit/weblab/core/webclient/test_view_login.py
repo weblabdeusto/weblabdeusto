7a6dd5a17f73788a3ac9025ce4d61db6fb2df603
Luis Rodriguez-Gil <luis.rodriguezgil@deusto.es>
2015-02-02 18:53:48 +0100

    [new_client]: Minor test-related adds

---
+++ server/src/test/unit/weblab/core/webclient/test_view_login.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+#-*-*- encoding: utf-8 -*-*-
+#
+# Copyright (C) 2005 onwards University of Deusto
+# All rights reserved.
+#
+# This software is licensed as described in the file COPYING, which
+# you should have received as part of this distribution.
+#
+# This software consists of contributions made by many individuals,
+# listed below:
+#
+# Author: Luis Rodriguez-Gil <luis.rodriguezgil@deusto.es>
+#
+import unittest
+
+
+class TestViewLogin(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
