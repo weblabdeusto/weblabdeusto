3be15c0013d7508c064919ecbae32a2b9bb2a038
Luis Rodriguez-Gil <luis.rodriguezgil@deusto.es>
2015-02-02 19:31:30 +0100

    [new_client]: Added with Pablo a weblab instance configuration for webclient tests, and the first (empty) webclient test.

---
+++ server/src/test/deployments/webclient_dummy/lab_config.py
@@ -0,0 +1,11 @@
+##################################
+# Laboratory Server configuration #
+##################################
+
+laboratory_assigned_experiments = {
+        'exp1:dummy1@Dummy experiments':
+            {
+                 'coord_address': 'myexperiment:myprocess@myhost',
+                 'checkers': ()
+            },
+    }
--- server/src/test/unit/weblab/core/webclient/test_view_login.py
+++ server/src/test/unit/weblab/core/webclient/test_view_login.py
@@ -11,14 +11,30 @@
 # listed below:
 #
 # Author: Luis Rodriguez-Gil <luis.rodriguezgil@deusto.es>
+#         Pablo Orduña <pablo.orduna@deusto.es>
 #
 import unittest
-
+from voodoo.gen import load_dir
+from voodoo.gen.registry import GLOBAL_REGISTRY
 
 class TestViewLogin(unittest.TestCase):
 
     def setUp(self):
-        pass
+
+        # Load the configuration of the weblab instance that we have set up for just this test.
+        self.global_config = load_dir('test/deployments/webclient_dummy')
+
+        # Start the weblab instance. Because we have activated the dont-start flag it won't actually
+        # start listening on the port, but let us use the Flask test methods instead.
+        self.handler = self.global_config.load_process('myhost', 'myprocess')
+
+        self.core_server = GLOBAL_REGISTRY['mycore:myprocess@myhost']
+        self.app = self.core_server.app.test_client()
+
+    def test_client(self):
+        rv = self.app.get('/weblab/login/web/login/?username=any&password=password')
+        print rv.data
 
     def tearDown(self):
-        pass
+        self.handler.stop()
+
