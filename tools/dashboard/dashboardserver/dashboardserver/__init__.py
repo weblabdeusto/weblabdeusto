46f999706573755b75d90a3ae28f9a7858d25380
Luis Rodriguez-Gil <luis.rodriguezgil@deusto.es>
2015-02-26 15:00:25 +0100

    [dashboard]: Added arquimedes board and cam checks to generic tests

--- tools/dashboard/dashboardserver/dashboardserver/__init__.py
+++ tools/dashboard/dashboardserver/dashboardserver/__init__.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from flask import jsonify
 from flask.ext.cors import cross_origin
 import flask_app_builder
@@ -7,7 +8,6 @@ flask_app = flask_app_builder.build_flask_app()
 api = restful.Api(flask_app)
 
 import checks
-
 
 @flask_app.route("/")
 def index():
@@ -23,29 +23,27 @@ def index():
 @flask_app.route("/status")
 @cross_origin()
 def status():
-    data = {}
-    data["archimedes"] = {
-        "webcamtest1" : {
-            "text": "First webcam is working",
-            "status": "OK",
-            "date": "2015-02-23",
-            "task_state": "finished"
-        }
-    }
+    data = defaultdict(dict)
+    # data["archimedes"] = {
+    #     "webcamtest1" : {
+    #         "text": "First webcam is working",
+    #         "status": "OK",
+    #         "date": "2015-02-23",
+    #         "task_state": "finished"
+    #     }
+    # }
 
     # Retrieve the full list of active checks in the REDIS instance.
-    check_ids = set([check_id.split(':')[2] for check_id in checks.r.keys("dashboard:checks:*.*:")])
+    check_ids = set([check_id.split(':')[2] for check_id in checks.r.keys("dashboard:checks:*.*:*")])
 
     for check_id in check_ids:
         check_data = checks.read_check(check_id)
-        component, id = check_id.split('.', 1)
-        data["component"] = {
-            id: {
+        component_id, element_id = check_id.split('.', 1)
+        data[component_id][element_id] = {
                 "text": check_data["msg"],
                 "status": check_data["result"],
                 "date": check_data["finished_date"],
                 "task_state": check_data["task_state"]
-            }
         }
 
     return jsonify(data)
