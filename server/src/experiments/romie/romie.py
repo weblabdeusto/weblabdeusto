609520a95723ca7c0566497c6ce48a8c3ba979b8
Razican <admin@razican.com>
2015-03-04 15:33:06 +0100

    Fixed max points not being saved

--- server/src/experiments/romie/romie.py
+++ server/src/experiments/romie/romie.py
@@ -158,9 +158,9 @@ class RoMIExperiment(Experiment.Experiment):
 
         elif command == 'FINISH':
 
-            conn = sqlite3.connect(self.database)
-            conn.execute("UPDATE forotech SET points = ? WHERE username = ?", (self.points, self.username,))
+            self.update_points()
 
+            conn = sqlite3.connect(self.database)
             cur = conn.cursor()
             cur.execute('SELECT username, name, surname, school, points FROM forotech WHERE username LIKE "forotech%" ORDER BY points DESC LIMIT 10')
             result = cur.fetchall()
@@ -198,8 +198,8 @@ class RoMIExperiment(Experiment.Experiment):
         points = cur.fetchone()[0]
 
         if (points < self.points):
-	        conn.execute("UPDATE forotech SET points = ? WHERE username = ?", (self.points, self.username,))
-	        conn.commit()
+            conn.execute("UPDATE forotech SET points = ? WHERE username = ?", (self.points, self.username,))
+            conn.commit()
 
         conn.close()
 
