25e41131a7e66414956b9d5b09c579cea625cf55
Luis Rodriguez-Gil <luis.rodriguezgil@deusto.es>
2015-03-10 13:55:25 +0100

    [ngdummy]: Webcam implemented, all in a very pretty way

--- client/src/es/deusto/weblab/public/jslabs/ngdummy/app/button/button.directive.js
+++ client/src/es/deusto/weblab/public/jslabs/ngdummy/app/button/button.directive.js
@@ -15,3 +15,5 @@ function wlButton() {
         controllerAs: "buttonController"
     }
 }
+
+
--- client/src/es/deusto/weblab/public/jslabs/ngdummy/app/webcam/webcam.directive.js
+++ client/src/es/deusto/weblab/public/jslabs/ngdummy/app/webcam/webcam.directive.js
@@ -1,14 +1,45 @@
-
 angular
     .module("ngdummy")
     .directive("wlWebcam", wlWebcam);
 
-function wlWebcam() {
+function wlWebcam($timeout) {
     return {
         restrict: "E",
         scope: {
-            ident: "=ident"
+            src: "@src"
         },
-        template: "Hello"
-    }
-}
+        templateUrl: "app/webcam/webcam.directive.html",
+        controller: "WebcamController",
+        controllerAs: "webcamController",
+        link: webcamLink
+    };
+
+    function webcamLink(scope, elem, attrs, ctrl) {
+
+        /// ---------------
+        /// Bindings
+        /// ---------------
+        elem.find("img").bind("load error", handleLoadAndError);
+
+
+        /// ---------------
+        /// Implementations
+        /// ---------------
+
+        function handleLoadAndError() {
+            ctrl.programRefresh();
+        }
+
+    } // ! webcamLink
+
+} // !wlWebcam
+
+
+
+
+
+
+
+
+
+
