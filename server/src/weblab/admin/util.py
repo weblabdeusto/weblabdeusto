fb0c3724443569420e31398dba4a63bb9448d83f
Pablo Orduña <pablo.orduna@deusto.es>
2015-07-06 01:11:16 +0200

    Try to fix the build

--- server/src/weblab/admin/util.py
+++ server/src/weblab/admin/util.py
@@ -1,11 +1,12 @@
 import sha
 import random
 
-def password2sha(password):
-    randomstuff = ""
-    for _ in range(4):
-        c = chr(ord('a') + random.randint(0, 25))
-        randomstuff += c
+def password2sha(password, randomstuff = None):
+    if randomstuff is None:
+        randomstuff = ""
+        for _ in range(4):
+            c = chr(ord('a') + random.randint(0, 25))
+            randomstuff += c
     password = password if password is not None else ''
     return randomstuff + "{sha}" + sha.new(randomstuff + password).hexdigest()
 
