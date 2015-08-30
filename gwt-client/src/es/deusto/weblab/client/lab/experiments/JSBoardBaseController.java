346055a3e9fb8c074ed22876ff0c3df360cd19d3
Pablo Orduña <pablo.orduna@deusto.es>
2015-08-31 01:23:32 +0200

    Implement first JS method (not yet tested)

--- gwt-client/src/es/deusto/weblab/client/lab/experiments/JSBoardBaseController.java
+++ gwt-client/src/es/deusto/weblab/client/lab/experiments/JSBoardBaseController.java
@@ -14,11 +14,53 @@
 
 package es.deusto.weblab.client.lab.experiments;
 
+import es.deusto.weblab.client.comm.exceptions.CommException;
 import es.deusto.weblab.client.dto.experiments.Command;
+import es.deusto.weblab.client.dto.experiments.ResponseCommand;
 import es.deusto.weblab.client.lab.comm.UploadStructure;
 import es.deusto.weblab.client.lab.comm.callbacks.IResponseCommandCallback;
 
 public class JSBoardBaseController implements IBoardBaseController {
+	
+	private final static ISimpleResponseCallback NULL_RESPONSE_CALLBACK = new ISimpleResponseCallback() {
+		@Override
+		public void onFailure(String e) {
+		}
+		
+		@Override
+		public void onSuccess(String responseCommand) {
+		}
+	};
+	
+	@Override
+	public void sendCommand(Command command) {
+		sendCommandImpl(command.getCommandString(), NULL_RESPONSE_CALLBACK);
+	}
+	
+	@Override
+	public void sendCommand(Command command, IResponseCommandCallback callback) {
+		sendCommandImpl(command.getCommandString(), new CallbackWrapper(callback));
+	}
+	
+	@Override
+	public void sendCommand(String command) {
+		sendCommandImpl(command, NULL_RESPONSE_CALLBACK);
+	}
+
+	@Override
+	public void sendCommand(String command, IResponseCommandCallback callback) {
+		sendCommandImpl(command, new CallbackWrapper(callback));
+	}
+
+	public static native void sendCommandImpl(String commandString, ISimpleResponseCallback callback) /*-{
+		WeblabExp.sendCommand(commandString)
+			.done(function(success) {
+				callback.@es.deusto.weblab.client.lab.experiments.ISimpleResponseCallback::onSuccess(Ljava/lang/String;)(success);
+			})
+			.fail(function(error) {
+				callback.@es.deusto.weblab.client.lab.experiments.ISimpleResponseCallback::onFailure(Ljava/lang/String;)(error);
+			});
+	}-*/;
 
 	@Override
 	public boolean isFacebook() {
@@ -26,29 +68,6 @@ public class JSBoardBaseController implements IBoardBaseController {
 		return false;
 	}
 
-	@Override
-	public void sendCommand(Command command) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void sendCommand(Command command, IResponseCommandCallback callback) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void sendCommand(String command) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void sendCommand(String command, IResponseCommandCallback callback) {
-		// TODO Auto-generated method stub
-
-	}
 
 	@Override
 	public void sendAsyncCommand(Command command) {
@@ -112,5 +131,24 @@ public class JSBoardBaseController implements IBoardBaseController {
 	public void disableFinishOnClose() {
 		// TODO Auto-generated method stub
 
+	}
+
+	public final static class CallbackWrapper implements ISimpleResponseCallback {
+		
+		private final IResponseCommandCallback callback;
+		
+		public CallbackWrapper(IResponseCommandCallback callback) {
+			this.callback = callback;
+		}
+		
+		@Override
+		public void onFailure(String e) {
+			this.callback.onFailure(new CommException(e));
+		}
+		
+		@Override
+		public void onSuccess(String responseCommand) {
+			this.callback.onSuccess(new ResponseCommand(responseCommand));
+		}
 	}
 }
