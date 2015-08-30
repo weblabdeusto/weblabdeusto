308891a3ba9ad92ea1018f8350bd2a2257096654
Pablo Orduña <pablo.orduna@deusto.es>
2015-08-30 19:44:12 +0200

    Add first embeddable GWT client for legacy experiments

---
+++ gwt-client/src/es/deusto/weblab/client/JSBoardBaseController.java
@@ -0,0 +1,124 @@
+/*
+* Copyright (C) 2012 onwards University of Deusto
+* All rights reserved.
+*
+* This software is licensed as described in the file COPYING, which
+* you should have received as part of this distribution.
+*
+* This software consists of contributions made by many individuals, 
+* listed below:
+*
+* Author: FILLME
+*
+*/
+
+package es.deusto.weblab.client;
+
+import es.deusto.weblab.client.dto.experiments.Command;
+import es.deusto.weblab.client.dto.experiments.ExperimentID;
+import es.deusto.weblab.client.lab.comm.UploadStructure;
+import es.deusto.weblab.client.lab.comm.callbacks.IResponseCommandCallback;
+import es.deusto.weblab.client.lab.experiments.IBoardBaseController;
+
+public class JSBoardBaseController implements IBoardBaseController {
+
+	@Override
+	public boolean isFacebook() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public ExperimentID getExperimentId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void sendCommand(Command command) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void sendCommand(Command command, IResponseCommandCallback callback) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void sendCommand(String command) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void sendCommand(String command, IResponseCommandCallback callback) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void sendAsyncCommand(Command command) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void sendAsyncCommand(Command command,
+			IResponseCommandCallback callback) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void sendAsyncCommand(String command) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void sendAsyncCommand(String command,
+			IResponseCommandCallback callback) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void sendFile(UploadStructure uploadStructure,
+			IResponseCommandCallback callback) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void sendAsyncFile(UploadStructure uploadStructure,
+			IResponseCommandCallback callback) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void clean() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void finish() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void stopPolling() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void disableFinishOnClose() {
+		// TODO Auto-generated method stub
+
+	}
+}
--- gwt-client/src/es/deusto/weblab/client/WebLabClient.java
+++ gwt-client/src/es/deusto/weblab/client/WebLabClient.java
@@ -14,13 +14,98 @@
 package es.deusto.weblab.client;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.Widget;
 
-public abstract class WebLabClient implements EntryPoint {
+import es.deusto.weblab.client.configuration.IConfigurationRetriever;
+import es.deusto.weblab.client.lab.experiments.EntryRegistry;
+import es.deusto.weblab.client.lab.experiments.ExperimentBase;
+import es.deusto.weblab.client.lab.experiments.ExperimentCreator;
+import es.deusto.weblab.client.lab.experiments.ExperimentFactory.IExperimentLoadedCallback;
+import es.deusto.weblab.client.lab.experiments.IBoardBaseController;
+import es.deusto.weblab.client.lab.experiments.IExperimentCreatorFactory;
+import es.deusto.weblab.client.lab.experiments.exceptions.ExperimentCreatorInstanciationException;
+
+public class WebLabClient implements EntryPoint {
     
 	public static String baseLocation;
+	private static final String MAIN_SLOT = "weblab_slot";
 	
+    public void putWidget(Widget widget){
+        while(RootPanel.get(WebLabClient.MAIN_SLOT).getWidgetCount() > 0)
+            RootPanel.get(WebLabClient.MAIN_SLOT).remove(0);
+        RootPanel.get(WebLabClient.MAIN_SLOT).add(widget);
+    }
+    
+    public void showError(String message) {
+    	putWidget(new Label(message));
+    }
+
 	@Override
 	public void onModuleLoad() {
-		this.baseLocation = ""; // TODO
+		WebLabClient.baseLocation = getBaseLocation();
+		final IBoardBaseController boardBaseController = new JSBoardBaseController();
+		final IConfigurationRetriever configurationRetriever = null;
+		
+		final String clientCodeName = getClientCodeName();
+		final IExperimentCreatorFactory experimentCreatorFactory = getExperimentFactory(clientCodeName);
+		if (experimentCreatorFactory == null) {
+			showError("client code name " + clientCodeName + " not implemented in GWT");
+			return;
+		}
+		
+		final ExperimentCreator creator;
+		try {
+			creator = experimentCreatorFactory.createExperimentCreator(configurationRetriever);
+		} catch (ExperimentCreatorInstanciationException e1) {
+			showError("Could not instantiate experiment: " + e1.getMessage());
+			e1.printStackTrace();
+			return;
+		}
+
+		final IExperimentLoadedCallback callback = new IExperimentLoadedCallback() {
+			
+			@Override
+			public void onFailure(Throwable e) {
+			}
+			
+			@Override
+			public void onExperimentLoaded(ExperimentBase experiment) {
+				WebLabClient.this.putWidget(experiment.getWidget());
+			}
+		};
+
+		if (getMobile()) {
+			creator.createMobile(boardBaseController, callback);
+		} else {
+			creator.createWeb(boardBaseController, callback);
+		}
 	}
+
+	private IExperimentCreatorFactory getExperimentFactory(final String clientCodeName) {
+		IExperimentCreatorFactory currentCreatorFactory = null;
+		for(IExperimentCreatorFactory creatorFactory : EntryRegistry.creatorFactories) {
+			if (creatorFactory.getCodeName().equals(clientCodeName)) {
+				currentCreatorFactory = creatorFactory;
+			}
+		}
+		return currentCreatorFactory;
+	}
+	
+	/*
+	 * JavaScript native implementations
+	 */
+	
+	public static native String getClientCodeName() /*-{
+		return $wnd.gwt_experiment_config.client_code_name;
+	}-*/; 
+	
+	public static native boolean getMobile() /*-{
+		return $wnd.gwt_experiment_config.mobile;
+	}-*/;
+	
+	public static native String getBaseLocation() /*-{
+		return $wnd.gwt_experiment_config.base_location;
+	}-*/;
 }
