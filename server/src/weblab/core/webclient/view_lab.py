9e8eade47d522b2a42f39dddaf352a78835a9195
Pablo Orduña <pablo.orduna@deusto.es>
2015-09-21 17:42:21 +0200

    Improve error display

--- server/src/weblab/core/webclient/view_lab.py
+++ server/src/weblab/core/webclient/view_lab.py
@@ -21,29 +21,31 @@ def lab(category_name, experiment_name):
     else:
         back_url = None
 
-    try:
-        experiment = None
-        if federated_mode:
-            weblab_api.ctx.reservation_id = federated_reservation_id
-            # Now obtain the current experiment
-            experiment = _get_experiment_data(weblab_api.api.get_reservation_experiment_info())
-        else:
+    experiment = None
+    if federated_mode:
+        weblab_api.ctx.reservation_id = federated_reservation_id
+        # Now obtain the current experiment
+        experiment = _get_experiment_data(weblab_api.api.get_reservation_experiment_info())
+    else:
+        try:
             experiment_list = weblab_api.api.list_experiments(experiment_name, category_name)
-            for exp_allowed in experiment_list:
-                if exp_allowed.experiment.name == experiment_name and exp_allowed.experiment.category.name == category_name:
-                    experiment = _get_experiment(exp_allowed)
+        except SessionNotFoundError:
+            flash(gettext("You are not logged in"), 'danger')
+            return redirect(url_for('.login', next=request.url))
 
-            if experiment is None:
-                # TODO: check what to do in case there is no session_id (e.g., federated mode)
-                if weblab_api.db.check_experiment_exists(experiment_name, category_name):
-                    flash(gettext("You don't have permission on this laboratory"), 'danger')
-                else:
-                    flash(gettext("Experiment does not exist"), 'danger')
-                return redirect(url_for('.labs'))
+        for exp_allowed in experiment_list:
+            if exp_allowed.experiment.name == experiment_name and exp_allowed.experiment.category.name == category_name:
+                experiment = _get_experiment(exp_allowed)
 
-        return render_template("webclient/lab.html", experiment=experiment, federated_mode = federated_mode, back_url = back_url)
-    except Exception as ex:
-        raise
+        if experiment is None:
+            # TODO: check what to do in case there is no session_id (e.g., federated mode)
+            if weblab_api.db.check_experiment_exists(experiment_name, category_name):
+                flash(gettext("You don't have permission on this laboratory"), 'danger')
+            else:
+                flash(gettext("Experiment does not exist"), 'danger')
+            return redirect(url_for('.labs'))
+
+    return render_template("webclient/lab.html", experiment=experiment, federated_mode = federated_mode, back_url = back_url)
 
 @weblab_api.route_webclient("/labs/<category_name>/<experiment_name>/config.json")
 def lab_config(category_name, experiment_name):
