9dfd505e22599f0281f8fd50df073f61f0feb351
Pablo Orduña <pablo.orduna@deusto.es>
2015-09-21 17:17:30 +0200

    Support that users coming from different routes are redirected to the particular route they should

--- server/src/weblab/core/webclient/view_federated.py
+++ server/src/weblab/core/webclient/view_federated.py
@@ -1,5 +1,6 @@
 from __future__ import print_function, unicode_literals
 
+import datetime
 from flask import render_template, request, flash, redirect, url_for, jsonify, session
 
 from weblab.core.wl import weblab_api
@@ -8,6 +9,7 @@ from weblab.core.wl import weblab_api
 
 @weblab_api.route_webclient('/federated/')
 def federated():
+    redirecting = session.pop('federated_redirecting', None)
     widget = request.args.get('widget')
     reservation_id = request.args.get('reservation_id')
     reservation_tokens = reservation_id.split(';')
@@ -15,9 +17,18 @@ def federated():
         reservation_id = reservation_tokens[0]
     else:
         reservation_id = reservation_tokens[0]
-        route = reservation_tokens[1]
-        # TODO: redirect to the same URL but using a cookie token
-        print(route)
+        reservation_id_plus_route = reservation_tokens[1]
+        if '.' in reservation_id_plus_route:
+            route = reservation_id_plus_route.split('.', 1)[1]
+            if route != weblab_api.ctx.route:
+                if redirecting:
+                    return "Invalid federated URL: you're attempting to use a route not used in this WebLab-Deusto instance"
+
+                session['federated_redirecting'] = "true"
+                response = redirect(request.url)
+                now = datetime.datetime.now()
+                response.set_cookie('weblabsessionid', reservation_id_plus_route, expires = now + datetime.timedelta(days = 100), path = weblab_api.ctx.location)
+                return response
     
     weblab_api.ctx.reservation_id = reservation_id
     try:
