a6d446b035f1409cc1f761a7f601806130f1125f
gabimart <gabi.martinez@deusto.es>
2016-06-24 15:17:15 +0200

    Started working on invitations

--- server/src/weblab/core/webclient/view_invitations.py
+++ server/src/weblab/core/webclient/view_invitations.py
@@ -1,11 +1,12 @@
 
 from flask import render_template, request, url_for, redirect, flash
+from flask.ext.wtf import validators
 
 from weblab.core.wl import weblab_api
 from weblab.core.i18n import gettext
 
-@weblab_api.route_webclient("/invitation/<id>", methods=["GET", "POST"])
-def invitation(id):
+@weblab_api.route_webclient("/invitation/<id>/register", methods=["GET", "POST"])
+def invitation_register(id):
 
     if request.method == "POST":
         # If this is a POST it is a login request.
@@ -22,7 +23,14 @@ def invitation(id):
         print "Password: {}".format(password)
         print "Verification: {}".format(verification)
         print "email: {}".format(email)
-        flash(gettext('Registration done'))
-        return redirect(url_for(".invitation", id=id,_external=True, _scheme=request.scheme))
+        return redirect(url_for(".invitation_register", id=id,_external=True, _scheme=request.scheme))
 
-    return render_template("webclient/invitation.html")
+    return render_template("webclient/registration_form.html")
+
+@weblab_api.route_webclient("/invitation/<id>", methods=["GET", "POST"])
+def invitation(id):
+    if request.method == "POST":
+        #Add user to the group here
+        return redirect(url_for(".login"))
+    return render_template("webclient/invitation.html", id=id)
+
