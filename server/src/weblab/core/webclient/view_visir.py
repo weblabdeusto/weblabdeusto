9aa47d502a5588f3453efe234fee3247b71e8141
Pablo Orduña <pablo@labsland.com>
2020-06-23 13:15:41 -0600

    Add fonts

--- server/src/weblab/core/webclient/view_visir.py
+++ server/src/weblab/core/webclient/view_visir.py
@@ -1,9 +1,15 @@
 from __future__ import print_function, unicode_literals
 
-from flask import request, render_template
+from werkzeug.utils import secure_filename
+from flask import request, render_template, current_app
 
 from weblab.core.wl import weblab_api
 
 @weblab_api.route_webclient('/visir.html')
 def visir():
     return render_template("webclient/visir.html")
+
+@weblab_api.route_webclient('/fonts/<path:path>')
+def fonts(path):
+    blueprint = current_app.blueprints['core_webclient']
+    return blueprint.send_static_file("gen/fonts/{}".format(secure_filename(path)))
